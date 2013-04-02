ec02ee93a34af248362c500861b86bced21c9ad3
James Ross <jross@bwh.harvard.edu>
2013-04-02 17:18:12 -0400

    Defined cip namespace around function definitions

--- Common/cipHelper.cxx
+++ Common/cipHelper.cxx
@@ -10,28 +10,28 @@
 
 #include "cipHelper.h"
 
-cip::LabelMapType::Pointer DownsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
+cip::LabelMapType::Pointer cip::DownsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
 {
   cip::LabelMapType::Pointer outputLabelMap;
 
   return outputLabelMap;
 }
 
-cip::LabelMapType::Pointer UpsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
+cip::LabelMapType::Pointer cip::UpsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
 {
   cip::LabelMapType::Pointer outputLabelMap;
   
   return outputLabelMap;
 }
 
-cip::CTType::Pointer UpsampleCT( short samplingAmount, cip::CTType::Pointer inputCT )
+cip::CTType::Pointer cip::UpsampleCT( short samplingAmount, cip::CTType::Pointer inputCT )
 {
   cip::CTType::Pointer outputCT;
 
   return outputCT;
 }
 
-cip::CTType::Pointer DownsampleCT( short samplingAmount, cip::CTType::Pointer inputCT )
+cip::CTType::Pointer cip::DownsampleCT( short samplingAmount, cip::CTType::Pointer inputCT )
 {
   CTType::Pointer outputCT;
 
