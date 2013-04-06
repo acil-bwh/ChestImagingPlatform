bbadf8e896f44ea8209bfabcdf2ea25bd922efd1
unknown <rolaharmouche@gmail.com>
2013-04-06 18:20:46 -0400

    completed and tested code for up and downsampling CT and label images

--- Common/cipHelper.cxx
+++ Common/cipHelper.cxx
@@ -8,21 +8,154 @@
 *	
 */
 
-
-//This comment has been added by Rola at 4/4/ 2:53 pm to test git.. and test again at 3:26
 #include "cipHelper.h"
+#include "itkResampleImageFilter.h"
+
 
 cip::LabelMapType::Pointer cip::DownsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
 {
-  cip::LabelMapType::Pointer outputLabelMap;
+	cip::LabelMapType::Pointer outputLabelMap;
 
-  return outputLabelMap;
+	typedef itk::IdentityTransform<double, 3> identityTransform;
+
+	// Code modified from //http://www.itk.org/Wiki/ITK/Examples/ImageProcessing/Upsampling
+
+	//
+	// Interpolate using a 3rd order B-pline
+	//
+	typedef itk::BSplineInterpolateImageFunction<LabelMapType, double, double> BSplineInterpolator;
+	typedef itk::ResampleImageFilter<LabelMapType, LabelMapType> ResampleFilter;
+
+	//
+	// Instantiate the transform, the b-spline interpolator and the resampler
+	//
+	identityTransform::Pointer idTransform = identityTransform::New();
+	idTransform->SetIdentity();
+
+	BSplineInterpolator::Pointer imageInterpolator = BSplineInterpolator::New();
+	imageInterpolator->SetSplineOrder(3);
+
+	ResampleFilter::Pointer resizeFilter = ResampleFilter::New();
+	resizeFilter->SetTransform(idTransform);
+	resizeFilter->SetInterpolator(imageInterpolator);
+	resizeFilter->SetOutputOrigin(inputLabelMap->GetOrigin());
+
+	//
+	// Compute and set the output spacing from the input spacing and samplingAmount 
+	//     
+	const LabelMapType::RegionType& inputRegion = inputLabelMap->GetLargestPossibleRegion();
+	const LabelMapType::SizeType& inputSize = inputRegion.GetSize();
+
+	unsigned int originalWidth = inputSize[0];
+	unsigned int originalLength = inputSize[1];
+	unsigned int originalHeight = inputSize[2];
+
+	unsigned int newWidth = (int)((double) originalWidth / (double) samplingAmount);
+	unsigned int newLength = (int)((double) originalLength / (double) samplingAmount);
+	unsigned int newHeight = (int)((double) originalHeight / (double) samplingAmount);
+
+	const LabelMapType::SpacingType& inputSpacing = inputLabelMap->GetSpacing();
+
+	double outputSpacing[3];
+	outputSpacing[0] = inputSpacing[0] * ((double) originalWidth / (double) newWidth);
+	outputSpacing[1] = inputSpacing[1] * ((double) originalLength / (double) newLength);
+	outputSpacing[2] = inputSpacing[2] * ((double) originalHeight / (double) newHeight);
+
+	std::cout<<"old dimensions are: "<< originalWidth<< " "<<originalLength<< " "<<originalHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< inputSpacing[0]<< " "<<inputSpacing[1]<< " "<<inputSpacing[2]<< " "<<std::endl;
+
+	std::cout<<"new dimensions are: "<< newWidth<< " "<<newLength<< " "<<newHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< outputSpacing[0]<< " "<<outputSpacing[1]<< " "<<outputSpacing[2]<< " "<<std::endl;
+	
+	//
+	// Set the resampler with the calculated parameters and resample
+	//
+	resizeFilter->SetOutputSpacing(outputSpacing);
+	itk::Size<3> outputSize = { {newWidth, newLength, newHeight} };
+	resizeFilter->SetSize(outputSize);
+	resizeFilter->SetInput(inputLabelMap);
+	resizeFilter->Update();
+
+	//
+	// Save the resampled output to the output image and return
+	//
+
+	outputLabelMap= resizeFilter->GetOutput();
+
+	std::cout<<"sucessully generaed volume"<<std::endl;
+	return outputLabelMap;
 }
 
 cip::LabelMapType::Pointer cip::UpsampleLabelMap( short samplingAmount, cip::LabelMapType::Pointer inputLabelMap )
 {
   cip::LabelMapType::Pointer outputLabelMap;
-  
+
+  typedef itk::IdentityTransform<double, 3> identityTransform;
+
+	// Code modified from //http://www.itk.org/Wiki/ITK/Examples/ImageProcessing/Upsampling
+
+	//
+	// Interpolate using a 3rd order B-pline
+	//
+	typedef itk::BSplineInterpolateImageFunction<LabelMapType, double, double> BSplineInterpolator;
+	typedef itk::ResampleImageFilter<LabelMapType, LabelMapType> ResampleFilter;
+
+	//
+	// Instantiate the transform, the b-spline interpolator and the resampler
+	//
+	identityTransform::Pointer idTransform = identityTransform::New();
+	idTransform->SetIdentity();
+
+	BSplineInterpolator::Pointer imageInterpolator = BSplineInterpolator::New();
+	imageInterpolator->SetSplineOrder(3);
+
+	ResampleFilter::Pointer resizeFilter = ResampleFilter::New();
+	resizeFilter->SetTransform(idTransform);
+	resizeFilter->SetInterpolator(imageInterpolator);
+	resizeFilter->SetOutputOrigin(inputLabelMap->GetOrigin());
+
+	//
+	// Compute and set the output spacing from the input spacing and samplingAmount 
+	//     
+	const LabelMapType::RegionType& inputRegion = inputLabelMap->GetLargestPossibleRegion();
+	const LabelMapType::SizeType& inputSize = inputRegion.GetSize();
+
+	unsigned int originalWidth = inputSize[0];
+	unsigned int originalLength = inputSize[1];
+	unsigned int originalHeight = inputSize[2];
+
+	unsigned int newWidth = (int)((double) originalWidth * (double) samplingAmount);
+	unsigned int newLength = (int)((double) originalLength * (double) samplingAmount);
+	unsigned int newHeight = (int)((double) originalHeight * (double) samplingAmount);
+
+	const LabelMapType::SpacingType& inputSpacing = inputLabelMap->GetSpacing();
+
+	double outputSpacing[3];
+	outputSpacing[0] = inputSpacing[0] * ((double) originalWidth / (double) newWidth);
+	outputSpacing[1] = inputSpacing[1] * ((double) originalLength / (double) newLength);
+	outputSpacing[2] = inputSpacing[2] * ((double) originalHeight / (double) newHeight);
+
+	std::cout<<"old dimensions are: "<< originalWidth<< " "<<originalLength<< " "<<originalHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< inputSpacing[0]<< " "<<inputSpacing[1]<< " "<<inputSpacing[2]<< " "<<std::endl;
+
+	std::cout<<"new dimensions are: "<< newWidth<< " "<<newLength<< " "<<newHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< outputSpacing[0]<< " "<<outputSpacing[1]<< " "<<outputSpacing[2]<< " "<<std::endl;
+	
+	//
+	// Set the resampler with the calculated parameters and resample
+	//
+	resizeFilter->SetOutputSpacing(outputSpacing);
+	itk::Size<3> outputSize = { {newWidth, newLength, newHeight} };
+	resizeFilter->SetSize(outputSize);
+	resizeFilter->SetInput(inputLabelMap);
+	resizeFilter->Update();
+
+	//
+	// Save the resampled output to the output image and return
+	//
+
+	outputLabelMap= resizeFilter->GetOutput();
+
   return outputLabelMap;
 }
 
@@ -30,13 +163,145 @@ cip::CTType::Pointer cip::UpsampleCT( short samplingAmount, cip::CTType::Pointer
 {
   cip::CTType::Pointer outputCT;
 
+  typedef itk::IdentityTransform<double, 3> identityTransform;
+
+	// Code modified from //http://www.itk.org/Wiki/ITK/Examples/ImageProcessing/Upsampling
+
+	//
+	// Interpolate using a 3rd order B-pline
+	//
+	typedef itk::BSplineInterpolateImageFunction<CTType, double, double> BSplineInterpolator;
+	typedef itk::ResampleImageFilter<CTType, CTType> ResampleFilter;
+
+	//
+	// Instantiate the transform, the b-spline interpolator and the resampler
+	//
+	identityTransform::Pointer idTransform = identityTransform::New();
+	idTransform->SetIdentity();
+
+	BSplineInterpolator::Pointer imageInterpolator = BSplineInterpolator::New();
+	imageInterpolator->SetSplineOrder(3);
+
+	ResampleFilter::Pointer resizeFilter = ResampleFilter::New();
+	resizeFilter->SetTransform(idTransform);
+	resizeFilter->SetInterpolator(imageInterpolator);
+	resizeFilter->SetOutputOrigin(inputCT->GetOrigin());
+
+	//
+	// Compute and set the output spacing from the input spacing and samplingAmount 
+	//     
+	const CTType::RegionType& inputRegion = inputCT->GetLargestPossibleRegion();
+	const CTType::SizeType& inputSize = inputRegion.GetSize();
+
+	unsigned int originalWidth = inputSize[0];
+	unsigned int originalLength = inputSize[1];
+	unsigned int originalHeight = inputSize[2];
+
+	unsigned int newWidth = (int)((double) originalWidth * (double) samplingAmount);
+	unsigned int newLength = (int)((double) originalLength * (double) samplingAmount);
+	unsigned int newHeight = (int)((double) originalHeight * (double) samplingAmount);
+
+	const CTType::SpacingType& inputSpacing = inputCT->GetSpacing();
+
+	double outputSpacing[3];
+	outputSpacing[0] = inputSpacing[0] * ((double) originalWidth / (double) newWidth);
+	outputSpacing[1] = inputSpacing[1] * ((double) originalLength / (double) newLength);
+	outputSpacing[2] = inputSpacing[2] * ((double) originalHeight / (double) newHeight);
+
+	std::cout<<"old dimensions are: "<< originalWidth<< " "<<originalLength<< " "<<originalHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< inputSpacing[0]<< " "<<inputSpacing[1]<< " "<<inputSpacing[2]<< " "<<std::endl;
+
+	std::cout<<"new dimensions are: "<< newWidth<< " "<<newLength<< " "<<newHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< outputSpacing[0]<< " "<<outputSpacing[1]<< " "<<outputSpacing[2]<< " "<<std::endl;
+	
+	//
+	// Set the resampler with the calculated parameters and resample
+	//
+	resizeFilter->SetOutputSpacing(outputSpacing);
+	itk::Size<3> outputSize = { {newWidth, newLength, newHeight} };
+	resizeFilter->SetSize(outputSize);
+	resizeFilter->SetInput(inputCT);
+	resizeFilter->Update();
+	//
+	// Save the resampled output to the output image and return
+	//
+	std::cout<<"sucessully generaed volume"<<std::endl;
+	outputCT= resizeFilter->GetOutput();
+
   return outputCT;
 }
 
 cip::CTType::Pointer cip::DownsampleCT( short samplingAmount, cip::CTType::Pointer inputCT )
 {
-  CTType::Pointer outputCT;
 
-  return outputCT;
+  cip::CTType::Pointer outputCT;
+
+	typedef itk::IdentityTransform<double, 3> identityTransform;
+
+	// Code modified from //http://www.itk.org/Wiki/ITK/Examples/ImageProcessing/Upsampling
+
+	//
+	// Interpolate using a 3rd order B-pline
+	//
+	typedef itk::BSplineInterpolateImageFunction<CTType, double, double> BSplineInterpolator;
+	typedef itk::ResampleImageFilter<CTType, CTType> ResampleFilter;
+
+	//
+	// Instantiate the transform, the b-spline interpolator and the resampler
+	//
+	identityTransform::Pointer idTransform = identityTransform::New();
+	idTransform->SetIdentity();
+
+	BSplineInterpolator::Pointer imageInterpolator = BSplineInterpolator::New();
+	imageInterpolator->SetSplineOrder(3);
+
+	ResampleFilter::Pointer resizeFilter = ResampleFilter::New();
+	resizeFilter->SetTransform(idTransform);
+	resizeFilter->SetInterpolator(imageInterpolator);
+	resizeFilter->SetOutputOrigin(inputCT->GetOrigin());
+
+	//
+	// Compute and set the output spacing from the input spacing and samplingAmount 
+	//     
+	const CTType::RegionType& inputRegion = inputCT->GetLargestPossibleRegion();
+	const CTType::SizeType& inputSize = inputRegion.GetSize();
+
+	unsigned int originalWidth = inputSize[0];
+	unsigned int originalLength = inputSize[1];
+	unsigned int originalHeight = inputSize[2];
+
+	unsigned int newWidth = (int)((double) originalWidth / (double) samplingAmount);
+	unsigned int newLength = (int)((double) originalLength / (double) samplingAmount);
+	unsigned int newHeight = (int)((double) originalHeight / (double) samplingAmount);
+
+	const CTType::SpacingType& inputSpacing = inputCT->GetSpacing();
+
+	double outputSpacing[3];
+	outputSpacing[0] = inputSpacing[0] * ((double) originalWidth / (double) newWidth);
+	outputSpacing[1] = inputSpacing[1] * ((double) originalLength / (double) newLength);
+	outputSpacing[2] = inputSpacing[2] * ((double) originalHeight / (double) newHeight);
+
+	std::cout<<"old dimensions are: "<< originalWidth<< " "<<originalLength<< " "<<originalHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< inputSpacing[0]<< " "<<inputSpacing[1]<< " "<<inputSpacing[2]<< " "<<std::endl;
+
+	std::cout<<"new dimensions are: "<< newWidth<< " "<<newLength<< " "<<newHeight<< " "<<std::endl;
+	std::cout<<"spacing is: "<< outputSpacing[0]<< " "<<outputSpacing[1]<< " "<<outputSpacing[2]<< " "<<std::endl;
+	
+	//
+	// Set the resampler with the calculated parameters and resample
+	//
+	resizeFilter->SetOutputSpacing(outputSpacing);
+	itk::Size<3> outputSize = { {newWidth, newLength, newHeight} };
+	resizeFilter->SetSize(outputSize);
+	resizeFilter->SetInput(inputCT);
+	resizeFilter->Update();
+
+	//
+	// Save the resampled output to the output image and return
+	//
+	outputCT= resizeFilter->GetOutput();
+	std::cout<<"returning volume"<<std::endl;
+	return outputCT;
+
 }
 
