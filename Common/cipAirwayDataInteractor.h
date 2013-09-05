da45bcf731dc0165d04e478df852b4d517242083
James Ross <jross@bwh.harvard.edu>
2013-09-05 16:02:22 -0400

    ENH: Edit airway particles now supports min-spanning tree-type labeling

--- Common/cipAirwayDataInteractor.h
+++ Common/cipAirwayDataInteractor.h
@@ -16,8 +16,10 @@
 #ifndef __cipAirwayDataInteractor_h
 #define __cipAirwayDataInteractor_h
 
+#include "vtkSmartPointer.h"
+#include "vtkBoostKruskalMinimumSpanningTree.h"
 #include "cipChestDataViewer.h"
-
+#include "vtkMutableUndirectedGraph.h"
 
 void InteractorKeyCallback( vtkObject*, unsigned long, void*, void* );
 
@@ -36,11 +38,37 @@ public:
 
   void RemoveActorAndRender( vtkActor* );
   void UpdateAirwayGenerationAndRender( vtkActor*, int );
-  void UndoUpdateAndRender ();
+  void UndoUpdateAndRender();
+  void SetRootNode( vtkActor* );
+  void SetIntermediateNode( vtkActor* );
+  void UpdateAirwayBranchCode( char );
+
+  /** Set airway particles poly data. Once read in, a minimum
+   *  spanning tree representation will be created. This
+   *  representation will enable labeling based on root node and
+   *  intermediate node specification: once a root node is specified,
+   *  a label will assigned to every node between a specified
+   *  intermediate node and the root node. */
+  void SetAirwayParticlesAsMinimumSpanningTree( vtkSmartPointer< vtkPolyData > );
 
 private:
+  void InitializeMinimumSpanningTree( vtkSmartPointer< vtkPolyData > );
+  bool GetEdgeWeight( unsigned int, unsigned int, vtkSmartPointer< vtkPolyData >, double* );
+  std::map< vtkActor*, unsigned int > ActorToParticleIDMap;
+  std::map< unsigned int, vtkActor* > ParticleIDToActorMap;
+
   vtkCallbackCommand* InteractorCallbackCommand;
   UndoState m_UndoState;      
+  vtkSmartPointer< vtkMutableUndirectedGraph > MinimumSpanningTree;
+  unsigned int NumberInputParticles;
+  unsigned int NumberOfPointDataArrays;
+  unsigned int MinimumSpanningTreeRootNode;
+  unsigned int MinimumSpanningTreeIntermediateNode;
+  double ParticleDistanceThreshold;
+  double EdgeWeightAngleSigma;
+
+  double* ActorColor;
+  std::string AirwayBranchCode;
 };
 
 #endif
