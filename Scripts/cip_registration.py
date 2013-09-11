2aee1d6e5537b4f18dc4a321d12fa70f83e760f3
rjosest <rjosest@gmail.com>
2013-09-10 20:44:29 -0400

    ENH: Initialized by aligning centroids. Added mutiple control options. Include a threshold-based body segmentation for the affine reg step

--- Scripts/cip_registration.py
+++ Scripts/cip_registration.py
@@ -15,6 +15,18 @@ parser.add_option("-s",dest="sigma",help="sigma for initial smoothing [default: 
 parser.add_option("-r",dest="rate",help="initial upsampling/downsampling rate [default: %default]", type="float", default=0.5)
 parser.add_option("--tmpDir",dest="tmpDirectory",help="temporary directory" )
 parser.add_option("--dataDir", dest="dataDirectory",help="data directory for the case IDs")
+parser.add_option("-d",dest="delete_cache",help="delete cache in temp directory [default: %default]", action="store_true")
+parser.add_option("-e",dest="elastic",help="run elastic", action="store_true")
+parser.add_option("-b",dest="use_body_mask",help="compute body mask for affine registration step", action="store_true")
+parser.add_option("-l",dest="use_lung_mask",help="employs lung mask for elastic registration step", action="store_true")
+parser.add_option("--bTh", dest="body_th", help="body intesity threshold [default: %default]", type="float", default=0)
+parser.add_option("--fast", dest="fast", help="fast (less accurate) registration", action="store_true")
+parser.add_option("--resampledOut", dest="resampled_out", help="produce resample outputs into the fixed and moving image space", action="store_true")
+
+#TODO:
+#Give an option to resample outputs
+#Give an option to compute a quality control image
+#Produce an output xml that conforms to our dtd
 
 (options, args) = parser.parse_args()
 
@@ -24,6 +36,13 @@ sigma = options.sigma
 rate = options.rate
 tmp_dir = options.tmpDirectory
 data_dir = options.dataDirectory
+delete_cache = options.delete_cache
+elastic = options.elastic
+use_lung_mask = options.use_lung_mask
+use_body_mask = options.use_body_mask
+body_th = options.body_th
+fast= options.fast
+resampled_out = options.resampled_out
 
 #Check required tools path enviroment variables for tools path
 toolsPaths = ['ANTS_PATH','TEEM_PATH','ITKTOOLS_PATH'];
@@ -56,6 +75,10 @@ moving_mask = os.path.join(data_dir,moving_cid + "_partialLungLabelMap.nhdr")
 fixed_mask_tmp = os.path.join(tmp_dir,fixed_cid + "_partialLungLabelMap.nhdr")
 moving_mask_tmp = os.path.join(tmp_dir,moving_cid + "_partialLungLabelMap.nhdr")
 
+fixed_body_mask_tmp = os.path.join(tmp_dir,fixed_cid + "_bodyMask.nhdr")
+moving_body_mask_tmp = os.path.join(tmp_dir,moving_cid + "_bodyMask.nhdr")
+
+
 #Conditions input images: Gaussian blurring to account for SHARP kernel
 #Compute tissue compartment volume (silly linear mapping)
 unu = os.path.join(path['TEEM_PATH'],"unu")
@@ -69,31 +92,47 @@ for im in [fixed,moving]:
   subprocess.call( tmp_command, shell=True)
 
 #Extract lung mask region and dilate result
-dilation_distance=[10,20]
-for kk,im in enumerate([fixed_mask,moving_mask]):
-  out = os.path.join(tmp_dir,os.path.basename(im))
-  tmp_command = unu + " 3op in_op 1 %(in)s 8 | " + unu + " resample -s x%(r)f x%(r)f x%(r)f -k cheap -o %(out)s"
-  tmp_command = tmp_command % {'in':im, 'out':out,'r':rate}
-  print tmp_command
-  sys.stdout.flush()
-  subprocess.call( tmp_command, shell=True)
-  tmp_command = "pxdistancetransform -m Maurer -in %(in)s -out %(out)s"
-  tmp_command = tmp_command % {'in':out, 'out':out}
-  tmp_command = os.path.join(path['ITKTOOLS_PATH'],tmp_command)
-  print tmp_command
-  sys.stdout.flush()
-  subprocess.call( tmp_command, shell=True)
-  tmp_command = "unu 2op lt %(in)s %(val)d -o %(out)s"
-  tmp_command = tmp_command % {'in':out, 'out':out,'val':dilation_distance[kk]}
-  tmp_command = os.path.join(path['TEEM_PATH'],tmp_command)
-  print tmp_command
-  sys.stdout.flush()
-  subprocess.call( tmp_command, shell=True)
+
+if use_lung_mask == True:
+  dilation_distance=[10,20]
+  for kk,im in enumerate([fixed_mask,moving_mask]):
+    out = os.path.join(tmp_dir,os.path.basename(im))
+    tmp_command = unu + " 3op in_op 1 %(in)s 10000 | " + unu + " resample -s x%(r)f x%(r)f x%(r)f -k cheap -o %(out)s"
+    tmp_command = tmp_command % {'in':im, 'out':out,'r':rate}
+    print tmp_command
+    sys.stdout.flush()
+    subprocess.call( tmp_command, shell=True)
+    tmp_command = "pxdistancetransform -m Maurer -in %(in)s -out %(out)s"
+    tmp_command = tmp_command % {'in':out, 'out':out}
+    tmp_command = os.path.join(path['ITKTOOLS_PATH'],tmp_command)
+    print tmp_command
+    sys.stdout.flush()
+    subprocess.call( tmp_command, shell=True)
+    tmp_command = "unu 2op lt %(in)s %(val)d -o %(out)s"
+    tmp_command = tmp_command % {'in':out, 'out':out,'val':dilation_distance[kk]}
+    tmp_command = os.path.join(path['TEEM_PATH'],tmp_command)
+    print tmp_command
+    sys.stdout.flush()
+    subprocess.call( tmp_command, shell=True)
+
+if use_body_mask == True:
+  for out_im,in_im in zip([fixed_body_mask_tmp,moving_body_mask_tmp],[fixed_tmp,moving_tmp]):
+    tmp_command = unu + " 2op gt %(in)s %(th)f -t short -o %(out)s"
+    tmp_command = tmp_command % {'in':in_im, 'out':out_im, 'th':(body_th-1000)/1055}
+    print tmp_command
+    sys.stdout.flush()
+    subprocess.call( tmp_command, shell=True)
+
 
 #Perform Affine registration
-percentage=0.2
-its="100x50x20,1e-6,5"
-tmp_command = "antsRegistration -d 3 -r [ %(f)s,%(m)s,1] \
+if fast == True:
+  percentage=0.1
+  its="100x20x0,1e-6,5"
+else:
+  percentage=0.3
+  its="100x50x20,1e-6,5"
+
+tmp_command = "antsRegistration -d 3 -r [ %(f)s,%(m)s,0] \
                -m mattes[  %(f)s, %(m)s , 1 , 32, regular, %(percentage)f ] \
                -t  translation[ 0.1 ] \
                -c [%(its)s,1.e-8,20]  \
@@ -102,60 +141,106 @@ tmp_command = "antsRegistration -d 3 -r [ %(f)s,%(m)s,1] \
                -l 1 \
                -m mattes[  %(f)s, %(m)s , 1 , 32, regular, %(percentage)f ] \
                -t affine[ 0.1 ] \
-               -c [%(its)s,1.e-8,20]  \
-               -s 4x2x1vox  \
+               -c [%(its)s,1.e-8,20] \
+               -s 4x2x1vox \
                -f 8x4x2 \
                -l 1 -u 1 -z 1 \
                -o [%(nm)s]"
 tmp_command = tmp_command % {'f':fixed_tmp,'m':moving_tmp, \
 'percentage':percentage,'its':its,'nm':deformation_prefix}
+
+if use_body_mask == True:
+  tmp_command = tmp_command + " -x [%(f_mask)s,%(m_mask)s]"
+  tmp_command = tmp_command % {'f_mask':fixed_body_mask_tmp,'m_mask':moving_body_mask_tmp}
+
 tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
 print tmp_command
 sys.stdout.flush()
 subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 
-#Perform Diffeomorphic registration
-syn="100x50x40,1e-6,5"
-tmp_command = "antsRegistration -d 3 -r %(affine)s \
--m cc[ %(f)s, %(m)s , 0.5 , 2 ] \
--t SyN[ .20, 3, 0 ] \
--c [ %(syn)s ]  \
--s 8x4x2vox  \
--f 8x4x2 \
--x [%(f_mask)s,%(m_mask)s] \
--l 1 -z 1 \
--o [%(nm)s,%(out)s,%(out_inv)s]"
-tmp_command = tmp_command % {'affine':affine_tfm, 'f':fixed_tmp, 'm':moving_tmp, \
-'syn':syn,'f_mask':fixed_mask_tmp,'m_mask':moving_mask_tmp, \
-'nm':deformation_prefix,'out':moving_deformed_tmp,'out_inv':fixed_deformed_tmp}
-tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
-print tmp_command
-sys.stdout.flush()
-subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+
+if elastic == True:
+  #Perform Diffeomorphic registration
+  if fast == True:
+    syn="100x20x0,1e-5,5"
+  else:
+    syn="100x50x40,1e-6,5"
+
+  tmp_command = "antsRegistration -d 3 -r %(affine)s \
+  -m cc[ %(f)s, %(m)s , 0.5 , 2 ] \
+  -t SyN[ .20, 3, 0 ] \
+  -c [ %(syn)s ]  \
+  -s 8x4x2vox  \
+  -f 8x4x2 \
+  -l 1 -z 1 \
+  -o [%(nm)s,%(out)s,%(out_inv)s]"
+  tmp_command = tmp_command % {'affine':affine_tfm, 'f':fixed_tmp, 'm':moving_tmp,'syn':syn, \
+  'nm':deformation_prefix,'out':moving_deformed_tmp,'out_inv':fixed_deformed_tmp}
+
+  if use_lung_mask == True:
+    tmp_command = tmp_command + " -x [%(f_mask)s,%(m_mask)s]"
+    tmp_command = tmp_command % {'f_mask':fixed_mask_tmp,'m_mask':moving_mask_tmp}
+
+  tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
+  print tmp_command
+  sys.stdout.flush()
+  subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+
 
 #Apply composite transform (if necessary)
 # The previous command already outputs the transformed images composed with both
 # transformations for the tmp images used in the registration. Here we deformed the original ones
 
-tmp_command = "antsApplyTransforms -d 3 -i %(m)s -r %(f)s -n linear \
-   -t %(elastic-tfm)s -t %(affine-tfm)s -o %(deformed)s"
-tmp_command = tmp_command % {'f':fixed,'m':moving, \
-'elastic-tfm':elastic_tfm,'affine-tfm':affine_tfm,'deformed':moving_deformed_tmp}
-tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
-subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+def transform_data ( reference_im, moving_im, transforms_list, output):
+  tmp_command = "antsApplyTransforms -d 3 -i %(m)s -r %(f)s -n linear -o %(deformed)s"
+  tmp_command = tmp_command % {'f':reference_im,'m':moving_im,'deformed':output}
+  for tt in transforms_list:
+    tmp_command = tmp_command + " -t %(tt)s"
+    tmp_command = tmp_command % {'tt':tt}
+  
+  tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
+  subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 
-tmp_command = "antsApplyTransforms -d 3 -i %(f)s -r %(m)s -n linear \
--t [%(affine-tfm)s,1] -t %(elastic-inv-tfm)s  -o %(deformed)s"
-tmp_command = tmp_command % {'f':fixed,'m':moving, \
-'elastic-inv-tfm':elastic_inv_tfm,'affine-tfm':affine_tfm,'deformed':fixed_deformed_tmp}
-tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
-subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+
+if resampled_out == True:
+  if elastic == True:
+    tt_list = [elastic_tfm,affine_tfm]
+    transform_data(fixed,moving,tt_list,moving_deformed_tmp)
+    tt_list = ["["+affine_tfm+",1]",elastic_inv_tfm]
+    transform_data(moving,fixed,tt_list,fixed_deformed_tmp)
+  else:
+    tt_list = [affine_tfm]
+    transform_data(fixed,moving,tt_list,moving_deformed_tmp)
+    tt_list = ["[" + affine_tfm + ",1]"]
+    transform_data(moving,fixed,tt_list,fixed_deformed_tmp)
+
+#tmp_command = "antsApplyTransforms -d 3 -i %(m)s -r %(f)s -n linear \
+#   -t %(elastic-tfm)s -t %(affine-tfm)s -o %(deformed)s"
+#tmp_command = tmp_command % {'f':fixed,'m':moving, \
+#'elastic-tfm':elastic_tfm,'affine-tfm':affine_tfm,'deformed':moving_deformed_tmp}
+#tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
+#subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+
+#tmp_command = "antsApplyTransforms -d 3 -i %(f)s -r %(m)s -n linear \
+#-t [%(affine-tfm)s,1] -t %(elastic-inv-tfm)s  -o %(deformed)s"
+#tmp_command = tmp_command % {'f':fixed,'m':moving, \
+#'elastic-inv-tfm':elastic_inv_tfm,'affine-tfm':affine_tfm,'deformed':fixed_deformed_tmp}
+#tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
+#subprocess.call( tmp_command, shell=True, env=os.environ.copy())
+
 
 # Do nrrd gzip compression and type casting for the output images
 for im_out,im_in in zip([fixed_deformed,moving_deformed],[fixed_deformed_tmp,moving_deformed_tmp]):
   tmp_command = unu + " convert -i %(in)s -t short | "+ unu + " save -f nrrd -e gzip -o %(out)s"
   tmp_command = tmp_command % {'in':im_in,'out':im_out}
   subprocess.call( tmp_command, shell=True)
+
+if delete_cache == True:
+  print "Cleaning tempoarary directory..."
+  tmp_command = "\\rm " + os.path.join(tmp_dir, "*")
+  subprocess.call( tmp_command, shell=True )
+
+
 
 
 
