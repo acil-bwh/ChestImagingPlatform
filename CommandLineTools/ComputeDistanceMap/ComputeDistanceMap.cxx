17ebd9dc57efd7769d3fb1aee83d6006298d503a
rola harmouche <rharmo@bwh.harvard.edu>
2013-10-19 17:54:20 -0400

    ENH: made modifications becessary to transform ComputeDistanceMap into a cli. also added licence to the older clis.

--- CommandLineTools/ComputeDistanceMap/ComputeDistanceMap.cxx
+++ CommandLineTools/ComputeDistanceMap/ComputeDistanceMap.cxx
@@ -42,7 +42,6 @@
  *
  */
 
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #include <tclap/CmdLine.h>
 #include "cipConventions.h"
@@ -56,7 +55,10 @@
 #include "itkSignedMaurerDistanceMapImageFilter.h"
 #include "itkCIPExtractChestLabelMapImageFilter.h"
 #include "itkSignedDanielssonDistanceMapImageFilter.h"
+#include "ComputeDistanceMapCLP.h"
 
+namespace
+{
 typedef itk::Image< short, 3 >                                                            DistanceMapType;
 typedef itk::ImageFileWriter< DistanceMapType >                                           WriterType;
 typedef itk::SignedMaurerDistanceMapImageFilter< cip::LabelMapType, DistanceMapType >     SignedMaurerType;
@@ -70,65 +72,104 @@ typedef itk::ImageRegionIteratorWithIndex< DistanceMapType >                    
 typedef itk::IdentityTransform< double, 3 >                                               IdentityType;
 
 
-cip::LabelMapType::Pointer ResampleImage( cip::LabelMapType::Pointer, float );
-DistanceMapType::Pointer ResampleImage( DistanceMapType::Pointer, cip::LabelMapType::SizeType, cip::LabelMapType::SpacingType );
+//cip::LabelMapType::Pointer ResampleImage( cip::LabelMapType::Pointer, float );
+//DistanceMapType::Pointer ResampleImage( DistanceMapType::Pointer, cip::LabelMapType::SizeType, cip::LabelMapType::SpacingType );
+    
+    cip::LabelMapType::Pointer ResampleImage( cip::LabelMapType::Pointer image, float downsampleFactor )
+    {
+        cip::LabelMapType::SizeType inputSize = image->GetBufferedRegion().GetSize();
+        
+        cip::LabelMapType::SpacingType inputSpacing = image->GetSpacing();
+        
+        cip::LabelMapType::SpacingType outputSpacing;
+        outputSpacing[0] = inputSpacing[0]*downsampleFactor;
+        outputSpacing[1] = inputSpacing[1]*downsampleFactor;
+        outputSpacing[2] = inputSpacing[2]*downsampleFactor;
+        
+        cip::LabelMapType::SizeType outputSize;
+        outputSize[0] = static_cast< unsigned int >( static_cast< double >( inputSize[0] )/downsampleFactor );
+        outputSize[1] = static_cast< unsigned int >( static_cast< double >( inputSize[1] )/downsampleFactor );
+        outputSize[2] = static_cast< unsigned int >( static_cast< double >( inputSize[2] )/downsampleFactor );
+        
+        NearestNeighborInterpolatorType::Pointer interpolator = NearestNeighborInterpolatorType::New();
+        
+        IdentityType::Pointer transform = IdentityType::New();
+        transform->SetIdentity();
+        
+        LabelMapResampleType::Pointer resampler = LabelMapResampleType::New();
+        resampler->SetTransform( transform );
+        resampler->SetInterpolator( interpolator );
+        resampler->SetInput( image );
+        resampler->SetSize( outputSize );
+        resampler->SetOutputSpacing( outputSpacing );
+        resampler->SetOutputOrigin( image->GetOrigin() );
+        try
+        {
+            resampler->Update();
+        }
+        catch ( itk::ExceptionObject &excp )
+        {
+            std::cerr << "Exception caught down sampling:";
+            std::cerr << excp << std::endl;
+        }
+        
+        return resampler->GetOutput();
+        
+    }
+    
+    
+    DistanceMapType::Pointer ResampleImage( DistanceMapType::Pointer image, cip::LabelMapType::SizeType outputSize, cip::LabelMapType::SpacingType outputSpacing )
+    {
+        //DistanceMapType::SizeType inputSize = image->GetBufferedRegion().GetSize();
+        
+        //DistanceMapType::SpacingType inputSpacing = image->GetSpacing();
+        
+        /*
+         DistanceMapType::SpacingType outputSpacing;
+         outputSpacing[0] = inputSpacing[0]*downsampleFactor;
+         outputSpacing[1] = inputSpacing[1]*downsampleFactor;
+         outputSpacing[2] = inputSpacing[2]*downsampleFactor;
+         
+         DistanceMapType::SizeType outputSize;
+         outputSize[0] = static_cast< unsigned int >( static_cast< double >( inputSize[0] )/downsampleFactor );
+         outputSize[1] = static_cast< unsigned int >( static_cast< double >( inputSize[1] )/downsampleFactor );
+         outputSize[2] = static_cast< unsigned int >( static_cast< double >( inputSize[2] )/downsampleFactor );
+         */
+        
+        LinearInterpolatorType::Pointer interpolator = LinearInterpolatorType::New();
+        
+        IdentityType::Pointer transform = IdentityType::New();
+        transform->SetIdentity();
+        
+        DistanceMapResampleType::Pointer resampler = DistanceMapResampleType::New();
+        resampler->SetTransform( transform );
+        resampler->SetInterpolator( interpolator );
+        resampler->SetInput( image );
+        resampler->SetSize( outputSize );
+        resampler->SetOutputSpacing( outputSpacing );
+        resampler->SetOutputOrigin( image->GetOrigin() );
+        try
+        {
+            resampler->Update();
+        }
+        catch ( itk::ExceptionObject &excp )
+        {
+            std::cerr << "Exception caught down sampling:";
+            std::cerr << excp << std::endl;
+        }
+        
+        
+        return resampler->GetOutput();
+        
+    } 
+
+} // end of anonymous namespace
 
 
 int main( int argc, char *argv[] )
 {
-  //
-  // Begin by defining the arguments to be passed
-  //
-  std::string   labelMapFileName    = "NA";
-  std::string   distanceMapFileName = "NA";
-  double        downsampleFactor    = 1.0;
-  bool          interiorIsPositive  = false;
 
-  //
-  // Argument descriptions for user 
-  //
-  std::string programDescription = "This program computes a distance map from an \
-  input binary map. A donwsampling can be applied prior to the distance map computation \
-  to improve performance. The resulting \
-  distance map will by upsampled by the same amount before writing.";
-   
-  std::string labelMapFileNameDescription = "Input label map file name";
-  std::string distanceMapFileNameDescription = "Output distance map file name";
-  std::string downsampleFactorDescription = "Downsample factor. The input label map will be \
-downsampled by the specified amount before the distance map is computed. The resulting \
-distance map will then be scaled up by the same amount before writing.";
-  std::string interiorIsPositiveDescription = "Set this flag to indicate that the interior \
-of the structure of interest should be assigned positive distance values";
-  
-  //
-  // Parse the input arguments
-  //
-  try
-    {
-    TCLAP::CmdLine cl( programDescription, ' ', "$Revision: 247 $" );
-
-    TCLAP::ValueArg<std::string>  labelMapFileNameArg ( "l", "labelMap", labelMapFileNameDescription, true, labelMapFileName, "string", cl );
-    TCLAP::ValueArg<std::string>  distanceMapFileNameArg ( "d", "distanceMap", distanceMapFileNameDescription, true, distanceMapFileName, "string", cl );
-    TCLAP::ValueArg<double>       downsampleFactorArg ( "s", "downsample", downsampleFactorDescription, false, downsampleFactor, "double", cl );
-    TCLAP::SwitchArg              interiorIsPositiveArg( "p", "interiorPositive", interiorIsPositiveDescription, false );
-
-    cl.parse( argc, argv );
-
-    labelMapFileName    = labelMapFileNameArg.getValue();
-    distanceMapFileName = distanceMapFileNameArg.getValue();
-    downsampleFactor    = downsampleFactorArg.getValue();
-    
-    if ( interiorIsPositiveArg.isSet() )
-      {
-      interiorIsPositive = true;
-      }
-
-    }
-  catch ( TCLAP::ArgException excp )
-    {
-    std::cerr << "Error: " << excp.error() << " for argument " << excp.argId() << std::endl;
-    return cip::ARGUMENTPARSINGERROR;
-    }  
+    PARSE_ARGS;
 
   //
   // Instantiate ChestConventions for convenience
@@ -218,93 +259,3 @@ of the structure of interest should be assigned positive distance values";
   return cip::EXITSUCCESS;
 }
 
-
-cip::LabelMapType::Pointer ResampleImage( cip::LabelMapType::Pointer image, float downsampleFactor )
-{
-  cip::LabelMapType::SizeType inputSize = image->GetBufferedRegion().GetSize();
-
-  cip::LabelMapType::SpacingType inputSpacing = image->GetSpacing();
-
-  cip::LabelMapType::SpacingType outputSpacing;
-    outputSpacing[0] = inputSpacing[0]*downsampleFactor;
-    outputSpacing[1] = inputSpacing[1]*downsampleFactor;
-    outputSpacing[2] = inputSpacing[2]*downsampleFactor;
-
-  cip::LabelMapType::SizeType outputSize;
-    outputSize[0] = static_cast< unsigned int >( static_cast< double >( inputSize[0] )/downsampleFactor );
-    outputSize[1] = static_cast< unsigned int >( static_cast< double >( inputSize[1] )/downsampleFactor );
-    outputSize[2] = static_cast< unsigned int >( static_cast< double >( inputSize[2] )/downsampleFactor );
-
-  NearestNeighborInterpolatorType::Pointer interpolator = NearestNeighborInterpolatorType::New();
-
-  IdentityType::Pointer transform = IdentityType::New();
-    transform->SetIdentity();
-
-  LabelMapResampleType::Pointer resampler = LabelMapResampleType::New();
-    resampler->SetTransform( transform );
-    resampler->SetInterpolator( interpolator );
-    resampler->SetInput( image );
-    resampler->SetSize( outputSize );
-    resampler->SetOutputSpacing( outputSpacing );
-    resampler->SetOutputOrigin( image->GetOrigin() );
-  try
-    {
-    resampler->Update();
-    }
-  catch ( itk::ExceptionObject &excp )
-    {
-    std::cerr << "Exception caught down sampling:";
-    std::cerr << excp << std::endl;
-    }
-   
-  return resampler->GetOutput();
-
-} 
-
-
-DistanceMapType::Pointer ResampleImage( DistanceMapType::Pointer image, cip::LabelMapType::SizeType outputSize, cip::LabelMapType::SpacingType outputSpacing )
-{
-  //DistanceMapType::SizeType inputSize = image->GetBufferedRegion().GetSize();
-
-  //DistanceMapType::SpacingType inputSpacing = image->GetSpacing();
-
-  /*
-  DistanceMapType::SpacingType outputSpacing;
-    outputSpacing[0] = inputSpacing[0]*downsampleFactor;
-    outputSpacing[1] = inputSpacing[1]*downsampleFactor;
-    outputSpacing[2] = inputSpacing[2]*downsampleFactor;
-  
-  DistanceMapType::SizeType outputSize;
-    outputSize[0] = static_cast< unsigned int >( static_cast< double >( inputSize[0] )/downsampleFactor );
-    outputSize[1] = static_cast< unsigned int >( static_cast< double >( inputSize[1] )/downsampleFactor );
-    outputSize[2] = static_cast< unsigned int >( static_cast< double >( inputSize[2] )/downsampleFactor );
-  */
-  
-  LinearInterpolatorType::Pointer interpolator = LinearInterpolatorType::New();
-
-  IdentityType::Pointer transform = IdentityType::New();
-    transform->SetIdentity();
-
-  DistanceMapResampleType::Pointer resampler = DistanceMapResampleType::New();
-    resampler->SetTransform( transform );
-    resampler->SetInterpolator( interpolator );
-    resampler->SetInput( image );
-    resampler->SetSize( outputSize );
-    resampler->SetOutputSpacing( outputSpacing );
-    resampler->SetOutputOrigin( image->GetOrigin() );
-  try
-    {
-    resampler->Update();
-    }
-  catch ( itk::ExceptionObject &excp )
-    {
-    std::cerr << "Exception caught down sampling:";
-    std::cerr << excp << std::endl;
-    }
-  
-  
-  return resampler->GetOutput();
-
-} 
-
-#endif
