abad8d0d5297fb4f33d7f24358caae4c3a655da7
rola harmouche <rharmo@bwh.harvard.edu>
2013-10-19 19:07:04 -0400

    ENH: Changed ConvertLabelMapValueToChestRegionChestType to CLI

--- CommandLineTools/ConvertLabelMapValueToChestRegionChestType/ConvertLabelMapValueToChestRegionChestType.cxx
+++ CommandLineTools/ConvertLabelMapValueToChestRegionChestType/ConvertLabelMapValueToChestRegionChestType.cxx
@@ -14,42 +14,12 @@
 
 #include <tclap/CmdLine.h>
 #include "cipConventions.h"
+#include "ConvertLabelMapValueToChestRegionChestTypeCLP.h"
 
 int main( int argc, char *argv[] )
 {
-  //
-  // Begin by defining the arguments to be passed
-  //
-  unsigned short value = 0;
-
-  //
-  // Input argument descriptions for user help
-  //
-  std::string programDesc = "This simple program takes an unsigned short label \
-map value that conforms to the labeling conventions laid out in \
-cipConventhions.h and writes to the command line the corresponding chest \
-region and chest type";
-
-  std::string valueDesc = "The unsigned short label map value";
-
-  //
-  // Parse the input arguments
-  //
-  try
-    {
-    TCLAP::CmdLine cl( programDesc, ' ', "$Revision: 325 $" );
-
-    TCLAP::ValueArg<unsigned short> valueArg ( "v", "", valueDesc, true, value, "unsigned short", cl );
-
-    cl.parse( argc, argv );
-
-    value = valueArg.getValue();
-    }
-  catch ( TCLAP::ArgException excp )
-    {
-    std::cerr << "Error: " << excp.error() << " for argument " << excp.argId() << std::endl;
-    return cip::ARGUMENTPARSINGERROR;
-    }
+    PARSE_ARGS;
+    
 
   cip::ChestConventions conventions;
 
