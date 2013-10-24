6e6ac749d29b5986dd0480dc8ee0152bb8f4c60d
rola harmouche <rharmo@bwh.harvard.edu>
2013-10-24 12:51:53 -0400

    ENH:added method to get a polynomial mapping from a given feature vector.

--- cip_python/segmentation/chest_structure_likelihoods.py
+++ cip_python/segmentation/chest_structure_likelihoods.py
@@ -1,6 +1,6 @@
 import pdb
 
-class ChestStructureLikelihoods:
+class Likelihoods:
     """Base class for representing likelihood terms for various chest
     structures of interest.
 
@@ -11,3 +11,25 @@ class ChestStructureLikelihoods:
     """
     def __init__(self):
       pass
+
+
+class ExponentialLikelihoods:
+    """Inhereted class for represening the lung likelihood term with a 2D
+    exponential function (features being intensity and distance).
+    
+    Parameters
+    ----------
+    w : array, shape ( D, 1 )
+        Weight vector for weighting components of feature vectors
+        
+    lambda : float
+        Exponential distribution parameter
+    
+    """
+    
+    def __init__(self):
+        self.coefficients = []
+        
+    def set_coefficients(self, list_of_coefficients):
+        self.coefficients =  list_of_coefficients
+        
