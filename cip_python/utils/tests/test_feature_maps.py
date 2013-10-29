c0c4e9d05245c9ddd62c454b50ecc131470037b3
James Ross <jross@bwh.harvard.edu>
2013-10-29 15:19:28 -0400

    Changes made to tests

--- cip_python/utils/tests/test_feature_maps.py
+++ cip_python/utils/tests/test_feature_maps.py
@@ -1,7 +1,7 @@
 import sys
-import cip_python.utils.feature_maps
+from cip_python.utils.feature_maps import PolynomialFeatureMap
 import numpy as np
-import Capture
+#import Capture
 
 #run nosetests --nocapture in utils directory
 
@@ -10,32 +10,32 @@ I2 = np.array([[[3,2,6,2],[4,5,9,100],[10,23,32,2]],[[1,5,2,4],[5,7,2,6],[8,2,3,
 I3 = np.array([[[3,2,6,2],[4,5,9,100],[10,23,32,2]],[[1,5,2,4],[5,7,2,6],[8,2,3,7]]])
 
 def test_polynomial_feature_map_num_terms():
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap(  [I1,I2],[1,2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1, I2], [1, 2])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 5
     
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap( [I1,I2], [0])  
+    my_polynomial_feature_map = PolynomialFeatureMap( [I1,I2], [0])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 1    
 
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2], [2,2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2], [2,2])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 3   
         
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2], [2,1,2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2], [2,1,2])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 5    
     
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2,I3], [2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2,I3], [2])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 6
     
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2,I3], [2,1,2,1,1])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2,I3], [2,1,2,1,1])  
     my_polynomial_feature_map.compute_num_terms()
     sys.stdout=my_polynomial_feature_map.num_terms
     assert my_polynomial_feature_map.num_terms == 9  
@@ -44,7 +44,7 @@ def test_polynomial_feature_map():
     sys.stderr.write(str(I1)+"\n")
     sys.stderr.write(str(I2)+"\n")
 
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap( [I1,I2],[1,2] )  
+    my_polynomial_feature_map = PolynomialFeatureMap( [I1,I2],[1,2] )  
     my_polynomial_feature_map.compute_num_terms()
     first_element = my_polynomial_feature_map.get_mapped_feature_vec_element(2)
     result = np.multiply(I1,I1)
@@ -54,20 +54,20 @@ def test_polynomial_feature_map():
     result = I1
     assert (first_element == result).all()
     
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap( [I1,I2], [0])  
+    my_polynomial_feature_map = PolynomialFeatureMap( [I1,I2], [0])  
     my_polynomial_feature_map.compute_num_terms()
     first_element = my_polynomial_feature_map.get_mapped_feature_vec_element(0)
     sys.stderr.write(str(first_element)+"\n")
     result = 1
     assert (first_element == result)   
 
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2], [2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2], [2])  
     my_polynomial_feature_map.compute_num_terms()
     first_element = my_polynomial_feature_map.get_mapped_feature_vec_element(1)
     result = np.multiply(I1,I2)
     assert (first_element == result).all() 
         
-    my_polynomial_feature_map = feature_maps.PolynomialFeatureMap([I1,I2], [2,2,1,2])  
+    my_polynomial_feature_map = PolynomialFeatureMap([I1,I2], [2,2,1,2])  
     my_polynomial_feature_map.compute_num_terms()
     first_element = my_polynomial_feature_map.get_mapped_feature_vec_element(1)
     result = I2
