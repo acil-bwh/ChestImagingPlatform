35ea3e2da4563af3ef21184fabb1aea4ae656232
James Ross <jross@bwh.harvard.edu>
2013-12-10 18:53:04 -0500

    ENH: SegmentLungLobes now a CLI

--- CommandLineTools/SegmentLungLobes/SegmentLungLobes.cxx
+++ CommandLineTools/SegmentLungLobes/SegmentLungLobes.cxx
@@ -65,7 +65,6 @@
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
-#include <tclap/CmdLine.h>
 #include "cipConventions.h"
 #include <fstream>
 #include "vtkPolyData.h"
@@ -79,98 +78,26 @@
 #include "cipThinPlateSplineSurface.h"
 #include "cipChestRegionChestTypeLocationsIO.h"
 #include <time.h>
+#include "SegmentLungLobesCLP.h"
 
 typedef cipLabelMapToLungLobeLabelMapImageFilter LungLobeSegmentationType;
 
 void AppendFissurePoints( std::vector< double* >*, vtkPolyData* );
 
-
 int main( int argc, char *argv[] )
 {
-  //
-  // Begin by defining the arguments to be passed
-  //
-  std::string leftLungRightLungFileName = "NA";
-  std::string loParticlesFileName       = "NA";
-  std::string roParticlesFileName       = "NA";
-  std::string rhParticlesFileName       = "NA";
-  std::string regionTypePointsFileName  = "NA";
-  std::string lobeLabelMapFileName      = "NA";
-  double      lambda                    = 0.1;
+  PARSE_ARGS;
 
-  //
-  // Program and argument descriptions for user help
-  //
-  std::string programDescription = "This program reads a label map image (where the labels\
-correspond to the conventions laid out cipChestConventions.h) as\
-well as particles defining the lobe boundaries and produces a\
-label map with the lung lobes identified. The input is assumed to\
-have the left and right lungs uniquely labeled. The user can pass\
-particles for the left lung only (left oblique fissure particles),\
-right lung only (both right oblique and right horizontal fissure\
-particles) or both. Thin plate splines are used to define the\
-interpolation boundaries between the lobes. It is assumed that the\
-input particles datasets are 'clean' in the sense that each\
-particle corresponds to (or is very likely to correspond to) the\
-fissure it represents.";
-
-  std::string leftLungRightLungFileNameDescription = "Left-lung-right-lung file name. The assumption for\
-this input label map is that the left and right lungs have been labeled correctly. Note that these are the\
-ONLY lung regions that are assumed to be labeled.";
-  std::string loParticlesFileNameDescription       = "Left oblique particles file name";
-  std::string roParticlesFileNameDescription       = "Right oblique particles file name";
-  std::string rhParticlesFileNameDescription       = "Right horizontal particles file name";
-  std::string regionTypePointsFileNameDescription  = "Region and type points file name";
-  std::string lobeLabelMapFileNameDescription      = "Lung lobe label map file name";
-  std::string lambdaDescription                    = "Thin plate spline smoothing parameter";
-
-  //
-  // Parse the input arguments
-  //
-  try
-    {
-    TCLAP::CmdLine cl( programDescription, ' ', "$Revision: 128 $" );
-
-    TCLAP::ValueArg<std::string> leftLungRightLungFileNameArg( "", "leftRight", leftLungRightLungFileNameDescription, true, leftLungRightLungFileName, "string", cl );
-    TCLAP::ValueArg<std::string> loParticlesFileNameArg( "", "loParticles", loParticlesFileNameDescription, false, loParticlesFileName, "string", cl );
-    TCLAP::ValueArg<std::string> roParticlesFileNameArg( "", "roParticles", roParticlesFileNameDescription, false, roParticlesFileName, "string", cl );
-    TCLAP::ValueArg<std::string> rhParticlesFileNameArg( "", "rhParticles", rhParticlesFileNameDescription, false, rhParticlesFileName, "string", cl );
-    TCLAP::ValueArg<std::string> regionTypePointsFileNameArg( "", "regionType", regionTypePointsFileNameDescription, false, regionTypePointsFileName, "string", cl );
-    TCLAP::ValueArg<std::string> lobeLabelMapFileNameArg( "", "lobes", lobeLabelMapFileNameDescription, true, lobeLabelMapFileName, "string", cl );
-    TCLAP::ValueArg<double>      lambdaArg( "l", "lambda", lambdaDescription, false, lambda, "double", cl );
-
-    cl.parse( argc, argv );
-
-    leftLungRightLungFileName = leftLungRightLungFileNameArg.getValue();
-    loParticlesFileName       = loParticlesFileNameArg.getValue();
-    roParticlesFileName       = roParticlesFileNameArg.getValue();
-    rhParticlesFileName       = rhParticlesFileNameArg.getValue();
-    regionTypePointsFileName  = regionTypePointsFileNameArg.getValue();
-    lobeLabelMapFileName      = lobeLabelMapFileNameArg.getValue();
-    lambda                    = lambdaArg.getValue();
-    }
-  catch ( TCLAP::ArgException excp )
-    {
-    std::cerr << "Error: " << excp.error() << " for argument " << excp.argId() << std::endl;
-    return cip::ARGUMENTPARSINGERROR;
-    }
-
-  //
   // Instatiate ChestConventions for general convenience later
-  //
   cip::ChestConventions conventions;
 
-  //
   // Define the vectors of physical points needed for creating the TPS
-  //
   std::vector< double* > loPoints;
   std::vector< double* > roPoints;
   std::vector< double* > rhPoints;
 
-  // 
   // If the user has specified a region-type points file name, read
   // the data
-  //
   if ( regionTypePointsFileName.compare( "NA" ) != 0 )
     {
     std::cout << "Reading region-type points file..." << std::endl;
@@ -178,10 +105,8 @@ ONLY lung regions that are assumed to be labeled.";
       regionTypesIO.SetFileName( regionTypePointsFileName );
       regionTypesIO.Read();
 
-    //
     // Loop through the points and identify those (if any) that
     // correspond to defined fissures
-    //
     unsigned char cipRegion;
     unsigned char cipType;
 
@@ -220,12 +145,10 @@ ONLY lung regions that are assumed to be labeled.";
       }
     }
 
-  //
   // Read in the left-lung-right-lung label map
-  //
   std::cout << "Reading lung label map..." << std::endl;
   cip::LabelMapReaderType::Pointer leftLungRightLungReader = cip::LabelMapReaderType::New();
-    leftLungRightLungReader->SetFileName( leftLungRightLungFileName );
+    leftLungRightLungReader->SetFileName( inLabelMapFileName );
   try
     {
     leftLungRightLungReader->Update();
@@ -238,9 +161,7 @@ ONLY lung regions that are assumed to be labeled.";
     return cip::LABELMAPREADFAILURE;
     }
 
-  //
   // Read in the particles data
-  //
   if ( loParticlesFileName.compare( "NA" ) != 0 )
     {
     std::cout << "Reading left oblique particles..." << std::endl;
@@ -271,11 +192,9 @@ ONLY lung regions that are assumed to be labeled.";
     AppendFissurePoints( &rhPoints, rhParticlesReader->GetOutput() );
     }
 
-  //
   // At this stage, we should have the necessary points for segmenting
   // the lobes: in the left lung, the right lung, or both. Before
   // proceeding, we need to verify this
-  //
   if ( roPoints.size() == 0 && rhPoints.size() == 0 && loPoints.size() == 0 )
     {
     std::cerr << "Insufficient fissure points specified. Exiting." << std::endl;
@@ -287,9 +206,7 @@ ONLY lung regions that are assumed to be labeled.";
     return cip::INSUFFICIENTDATAFAILURE;
     }
 
-  //
   // Now define the TPS surfaces for each of the three fissures
-  //
   cipThinPlateSplineSurface* roTPS = new cipThinPlateSplineSurface;
   if ( roPoints.size() != 0 )
     {
@@ -314,9 +231,7 @@ ONLY lung regions that are assumed to be labeled.";
     rhTPS->SetSurfacePoints( &rhPoints );
     }
 
-  //
   // Now segment the lobes
-  //
   std::cout << "Segmenting lobes..." << std::endl;
   LungLobeSegmentationType::Pointer lobeSegmenter = LungLobeSegmentationType::New();
     lobeSegmenter->SetInput( leftLungRightLungReader->GetOutput() );
@@ -331,14 +246,12 @@ ONLY lung regions that are assumed to be labeled.";
     }
     lobeSegmenter->Update();
       
-  //
   // Write the lung lobe label map 
-  //
   std::cout << "Writing lung lobe label map..." << std::endl;
   cip::LabelMapWriterType::Pointer writer = cip::LabelMapWriterType::New();
     writer->SetInput( lobeSegmenter->GetOutput() );
     writer->UseCompressionOn();
-    writer->SetFileName( lobeLabelMapFileName );
+    writer->SetFileName( outLabelMapFileName );
   try
     {
     writer->Update();
