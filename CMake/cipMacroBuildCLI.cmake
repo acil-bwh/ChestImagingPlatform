4ffb86ac440149f6d1beb786fb44d8629a9d25b9
Raul San Jose <rjosest@gmail.com>
2014-04-04 15:40:08 -0400

    NEW: Creating CMake directory to include CMake Macros and functions. Move files from root to this subdirectory

---
+++ CMake/cipMacroBuildCLI.cmake
@@ -0,0 +1,57 @@
+# SlicerExecutionModel
+find_package(SlicerExecutionModel NO_MODULE REQUIRED GenerateCLP)
+include(${SlicerExecutionModel_USE_FILE})
+
+FIND_PACKAGE( CIP REQUIRED )
+
+macro(cipMacroBuildCLI)
+  set(options
+    NO_INSTALL VERBOSE
+  )
+  
+  set (oneValueArgs
+     NAME 
+     LOGO_HEADER
+  )
+  
+  set(multiValueArgs
+    ADDITIONAL_TARGET_LIBRARIES
+    ADDITIONAL_INCLUDE_DIRECTORIES
+    SRCS
+  )
+  
+  CMAKE_PARSE_ARGUMENTS(MY_CIP
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"  
+    ${ARGN}
+    )
+
+  set ( INCLUDE_DIRECTORIES
+    ${CIP_INCLUDE_DIRECTORIES}
+    ${MY_CIP_ADDITIONAL_INCLUDE_DIRECTORIES}
+   )
+   
+   set(TARGET_LIBRARIES
+  ${MY_CIP_ADDITIONAL_TARGET_LIBRARIES}
+  ${ITK_LIBRARIES}
+  ${CIP_LIBRARIES}
+  )
+
+  
+  if(${CIP_BUILD_CLI_EXECUTABLEONLY})
+       set(PASS_EXECUTABLE_ONLY EXECUTABLE_ONLY)
+  endif()
+  
+  SEMMacroBuildCLI(
+       NAME ${MODULE_NAME}
+       LOGO_HEADER ${MY_CIP_LOGO_HEADER}
+       TARGET_LIBRARIES ${TARGET_LIBRARIES}
+       INCLUDE_DIRECTORIES ${INCLUDE_DIRECTORIES}
+       ADDITIONAL_SRCS ${SRCS}
+       LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+       RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+       ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+       ${PASS_EXECUTABLE_ONLY}
+  )
+endmacro()
