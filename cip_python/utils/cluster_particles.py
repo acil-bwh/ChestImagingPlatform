5d57ddb7da40851c9cc996c772961e4b0cf18c5f
Raul San Jose <rjosest@gmail.com>
2014-04-09 17:49:23 -0400

    FIX: Adding verts cell to particles vtkPolyData. ENH: Creating a class to do particle left/right splitting and labeling.

--- cip_python/utils/cluster_particles.py
+++ cip_python/utils/cluster_particles.py
@@ -55,18 +55,23 @@ class ClusterParticles:
     points=vtk_to_numpy(self._in_vtk.GetPoints().GetData())
 
     s_points=vtk.vtkPoints()
+    cell_arr=vtk.vtkCellArray()
     
     #s_points.SetData(numpy_to_vtk(points[ids,:]))
     #s_points.SetNumberOfPoints(s_points.GetData().GetNumberOfTuples())
     s_p = points[ids,:]
     s_points.SetNumberOfPoints(s_p.shape[0])
+    cell_arr.SetNumberOfCells(s_p.shape[0])
     for kk in xrange(s_p.shape[0]):
       s_points.SetPoint(kk,s_p[kk,0],s_p[kk,1],s_p[kk,2])
+      cell_arr.InsertNextCell(1)
+      cell_arr.InsertCellPoint(kk)
     
     data.SetPoints(s_points)
+    data.SetVerts(cell_arr)
+    
     #Transfer point data and field data
     for pd,out_pd in zip([self._in_vtk.GetPointData(),self._in_vtk.GetFieldData()],[data.GetPointData(),data.GetFieldData()]):
-      pd=self._in_vtk.GetPointData()
       for k in xrange(pd.GetNumberOfArrays()):
         arr=vtk_to_numpy(pd.GetArray(pd.GetArrayName(k)))
         if len(arr.shape) == 1:
@@ -76,6 +81,7 @@ class ClusterParticles:
         
         s_vtk_arr.SetName(pd.GetArrayName(k))
         out_pd.AddArray(s_vtk_arr)
+    
     return data
                       
     #Method to do the extraction using a vtk pipeline (experimental with seg fault)
@@ -96,7 +102,56 @@ class ClusterParticles:
       sel_filter.SetInput(1,sel)
       sel_filter.Update()
       return sel_filter.GetOutput()
-                      
+
+class LeftRightParticleLabeling():
+  def __init__(self,in_vtk):
+    self._in_vtk = in_vtk
+    self._out_vtk=dict()
+    self._out_vtk['left'] = None
+    self._out_vtk['right']= None
+    self._out_vtk['both']= None
+
+  def execute(self):
+    output_collection = vtk.vtkCollection()
+    cluster=ClusterParticles(input,output_collection)
+    cluster._number_of_clusters=2
+    cluster._method='MiniBatchKMeans'
+
+    cluster.execute()
+
+    if cluster._centroids[0,0] > cluster._centroids[1,0]:
+      tags=['left','right']
+      chest_region=[3,2]
+      chest_type=[3,3]
+    else:
+      tags=['right','left']
+      chest_region=[2,3]
+      chest_type=[3,3]
+
+    append=vtk.vtkAppendPolyData()
+    for k,tag,cr,ct in zip([0,1],tags,chest_region,chest_type):
+      self._out_vtk[tag]=output_collection.GetItemAsObject(k)
+      chest_region_arr = vtk.vtkUnsignedCharArray()
+      chest_region_arr.SetName('ChestRegion')
+      chest_type_arr = vtk.vtkUnsignedCharArray()
+      chest_type_arr.SetName('ChestType')
+      np = self._out_vtk[tag].GetNumberOfPoints()
+      chest_region_arr.SetNumberOfTuples(np)
+      chest_type_arr.SetNumberOfTuples(np)
+      for ii in xrange(self._out_vtk[tag].GetNumberOfPoints()):
+        chest_region_arr.SetValue(ii,cr)
+        chest_type_arr.SetValue(ii,ct)
+      self._out_vtk[tag].GetPointData().AddArray(chest_region_arr)
+      self._out_vtk[tag].GetPointData().AddArray(chest_type_arr)
+      
+      append.AddInput(self._out_vtk[tag])
+
+    append.Update()
+    self._out_vtk['both']=append.GetOutput()
+    return self._out_vtk
+
+      
+
 if __name__ == "__main__":
   desc = """Cluster particles points"""
   
@@ -110,6 +165,8 @@ if __name__ == "__main__":
                     dest='output_suffix',metavar='<string>',default='.vtk')
   parser.add_argument('-s',help='Split particles in left/right lung',
                       dest='split_flag',action="store_true",default=False)
+  parser.add_argument('-l',help='save left/rigth particles in a single vtk with region/type PointData array',
+                      dest='label_flag',default=False,action="store_true")
   
   op =  parser.parse_args()
   
@@ -119,32 +176,31 @@ if __name__ == "__main__":
   
   input=reader.GetOutput()
   
-  output_collection=vtk.vtkCollection()
-  cluster=ClusterParticles(input,output_collection)
-
   if op.split_flag == True:
-    cluster._number_of_clusters=2
-    cluster._method='MiniBatchKMeans'
-  else:
-    cluster._method='DBSCAN'
-
-  cluster.execute()
-
-  if op.split_flag == True:
-    #Find out which cluster is left vs right
-    if cluster._centroids[0,0] > cluster._centroids[1,0]:
-      tags=['left','right']
-    else:
-      tags=['right','left']
-    for k,tag in zip([0,1],tags):
+    labeler = LeftRightParticleLabeling(input)
+    output = labeler.execute()
+    if op.label_flag == True:
       writer=vtk.vtkPolyDataWriter()
-      writer.SetInput(output_collection.GetItemAsObject(k))
-      writer.SetFileName(op.output_prefix + '_%s%s' % (tag,op.output_suffix))
+      writer.SetInput(output['both'])
+      writer.SetFileTypeToBinary()
+      writer.SetFileName(op.output_prefix + op.output_suffix)
       writer.Update()
-  else: 
+    else:
+      for tag in ['right','left']:
+        writer=vtk.vtkPolyDataWriter()
+        writer.SetInput(output[tag])
+        writer.SetFileName(op.output_prefix + '_%s%s' % (tag,op.output_suffix))
+        writer.SetFileTypeToBinary()
+        writer.Update()
+  else:
+    output_collection=vtk.vtkCollection()
+    cluster=ClusterParticles(input,output_collection)
+    cluster._method='DBSCAN'
+    cluster.execute()
     for k in xrange(output_collection.GetNumberOfItems()):
       writer=vtk.vtkPolyDataWriter()
       writer.SetInput(output_collection.GetItemAsObject(k))
       writer.SetFileName(op.output_prefix + '_cluster%03d%s' % (k,op.output_suffix) )
+      writer.SetFileTypeToBinary()
       writer.Update()
 
