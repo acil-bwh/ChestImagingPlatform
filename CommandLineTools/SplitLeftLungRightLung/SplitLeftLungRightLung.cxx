929bf2f08f4cc0c1a9dfdcb10d2039340b9ac886
James Ross <jross@bwh.harvard.edu>
2015-01-03 17:35:32 -0500

    ENH: Code was previously commented out. Now the program is functional

--- CommandLineTools/SplitLeftLungRightLung/SplitLeftLungRightLung.cxx
+++ CommandLineTools/SplitLeftLungRightLung/SplitLeftLungRightLung.cxx
@@ -22,113 +22,75 @@
 
 namespace
 {
- //typedef itk::Image< short, 3 >                                    cip::CTType;
- typedef itk::Image< unsigned short, 3 >                           LabelMapType;
- typedef itk::ImageFileReader< LabelMapType >                      ReaderType;
- typedef itk::ImageFileWriter< LabelMapType >                      WriterType;
-typedef itk::CIPSplitLeftLungRightLungImageFilter< cip::CTType >   SplitterType;
+  typedef itk::CIPSplitLeftLungRightLungImageFilter< cip::CTType >  SplitterType;
 }
 
 int main( int argc, char *argv[] )
-{
-    
-    PARSE_ARGS;
-   //
-   // Begin by defining the arguments to be passed
-   //
-   //std::string inLabelMapFileName  = "NA";
-  // std::string outLabelMapFileName = "NA";
-  // double exponentialTimeConstant  = -700;
-  // double exponentialCoefficient   = 200.0;
+{  
+  PARSE_ARGS;
+  
+  std::cout << "Reading label map..." << std::endl;
+  cip::LabelMapReaderType::Pointer lmReader = cip::LabelMapReaderType::New();
+    lmReader->SetFileName( lmFileName );
+  try
+    {
+    lmReader->Update();
+    }
+  catch ( itk::ExceptionObject &excp )
+    {
+    std::cerr << "Exception caught reading lung label map:";
+    std::cerr << excp << std::endl;
 
-   //
-   // Descriptions for user help
-   //
- //  std::string programDescription = "This program reads a label map and splits the left and\
- right lungs so that they are uniquely labeled. If the input is\
- already split, the output will be identical to the input.";
+    return cip::LABELMAPREADFAILURE;
+    }
 
-  // std::string inLabelMapFileNameDescription = "Input label map file name";
-  // std::string outLabelMapFileNameDescription = "Output label map file name";
-  // std::string exponentialTimeConstantDescription = "Exponential time constant (-700 by default)";
- //  std::string exponentialCoefficientDescription = "Exponential coefficient (200 by default)";
+  std::cout << "Reading CT..." << std::endl;
+  cip::CTReaderType::Pointer ctReader = cip::CTReaderType::New();
+    ctReader->SetFileName( ctFileName );
+  try
+    {
+    ctReader->Update();
+    }
+  catch ( itk::ExceptionObject &excp )
+    {
+    std::cerr << "Exception caught reading CT:";
+    std::cerr << excp << std::endl;
 
-   //
-   // Parse the input arguments
-   //
-  /* try
-     {
-     //TCLAP::CmdLine cl( programDescription, ' ', "$Revision: 93 $" );
+    return cip::NRRDREADFAILURE;
+    }
 
-     //TCLAP::ValueArg<std::string> inLabelMapFileNameArg( "i", "in", inLabelMapFileNameDescription, true, inLabelMapFileName, "string", cl );
-     //TCLAP::ValueArg<std::string> outLabelMapFileNameArg( "o", "out", outLabelMapFileNameDescription, true, outLabelMapFileName, "string", cl );
-     //TCLAP::ValueArg<double> exponentialTimeConstantArg( "t", "timeConst", exponentialTimeConstantDescription, false, exponentialTimeConstant, "double", cl );
-    // TCLAP::ValueArg<double> exponentialCoefficientArg( "c", "coefficient", exponentialCoefficientDescription, false, exponentialCoefficient, "double", cl );
-
-     //cl.parse( argc, argv );
-
-     //inLabelMapFileName      = inLabelMapFileNameArg.getValue();
-     outLabelMapFileName     = outLabelMapFileNameArg.getValue();
-     exponentialTimeConstant = exponentialTimeConstantArg.getValue();
-     exponentialCoefficient  = exponentialCoefficientArg.getValue();
-     }
-   catch ( TCLAP::ArgException excp )
-     {
-     std::cerr << "Error: " << excp.error() << " for argument " << excp.argId() << std::endl;
-     return cip::ARGUMENTPARSINGERROR;
-     }
-*/
-    
-   std::cout << "Reading label map..." << std::endl;
-   ReaderType::Pointer labelMapReader = ReaderType::New();
-     labelMapReader->SetFileName( inLabelMapFileName );
+   std::cout << "Splitting..." << std::endl;
+   SplitterType::Pointer splitter = SplitterType::New();
+     splitter->SetInput( ctReader->GetOutput() );
+     splitter->SetLungLabelMap( lmReader->GetOutput() );
+     splitter->SetExponentialCoefficient( exponentialCoefficient );
+     splitter->SetExponentialTimeConstant( exponentialTimeConstant );
+     splitter->SetLeftRightLungSplitRadius( splitRadius );
    try
      {
-     labelMapReader->Update();
+     splitter->Update();
      }
    catch ( itk::ExceptionObject &excp )
      {
-     std::cerr << "Exception caught reading lung label map:";
+     std::cerr << "Exception caught splitting:";
      std::cerr << excp << std::endl;
-
-     return cip::LABELMAPREADFAILURE;
      }
 
- //   std::cout << "Splitting..." << std::endl;
- //   SplitterType::Pointer splitter = SplitterType::New();
- //     splitter->SetInput( reader->GetOutput() );
- //     splitter->SetExponentialCoefficient( exponentialCoefficient );
- //     splitter->SetExponentialTimeConstant( exponentialTimeConstant );
- //     splitter->SetLeftRightLungSplitRadius( leftRightSplitRadius );
- //   if ( aggressiveLeftRightSplitting == 1 )
- //     {
- //     splitter->SetAggressiveLeftRightSplitter( true );
- //     }
- //   try
- //     {
- //     splitter->Update();
- //     }
- //   catch ( itk::ExceptionObject &excp )
- //     {
- //     std::cerr << "Exception caught splitting:";
- //     std::cerr << excp << std::endl;
- //     }
-
- //   std::cout << "Writing split lung label map..." << std::endl;
- //   WriterType::Pointer writer = WriterType::New();
- //     writer->SetInput( splitter->GetOutput() );
- //     writer->SetFileName( outputFileName );
- //     writer->UseCompressionOn();
- //   try
- //     {
- //     writer->Update();
- //     }
- //   catch ( itk::ExceptionObject &excp )
- //     {
- //     std::cerr << "Exception caught while writing lung label map:";
- //     std::cerr << excp << std::endl;
- //     return cip::LABELMAPWRITEFAILURE;
- //     }
+   std::cout << "Writing split lung label map..." << std::endl;
+   cip::LabelMapWriterType::Pointer writer = cip::LabelMapWriterType::New();
+     writer->SetInput( splitter->GetOutput() );
+     writer->SetFileName( outFileName );
+     writer->UseCompressionOn();
+   try
+     {
+     writer->Update();
+     }
+   catch ( itk::ExceptionObject &excp )
+     {
+     std::cerr << "Exception caught while writing lung label map:";
+     std::cerr << excp << std::endl;
+     return cip::LABELMAPWRITEFAILURE;
+     }
 
    std::cout << "DONE." << std::endl;
 
