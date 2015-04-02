f9be21d377eba94f1be74afd82ea2875a6be8b36
James Ross <jross@bwh.harvard.edu>
2015-04-02 17:08:36 -0400

    Adding assignment operator and changing const-ness of methods

--- Common/cipGeometryTopologyData.cxx
+++ Common/cipGeometryTopologyData.cxx
@@ -5,6 +5,57 @@ cip::GeometryTopologyData::GeometryTopologyData()
 {
 }
 
+cip::GeometryTopologyData::~GeometryTopologyData(void)
+{
+  this->m_BoundingBoxes.clear();
+  this->m_Points.clear();
+}
+
+cip::GeometryTopologyData& cip::GeometryTopologyData::operator= (const cip::GeometryTopologyData &geometryTopology)
+{
+  // Check for self-assignment by comparing the address of the
+  // implicit object and the parameter
+  if (this == &geometryTopology)
+    {
+      return *this;
+    }
+  
+  // Do the copy
+  for ( unsigned int i=0; i<geometryTopology.GetNumberOfBoundingBoxes(); i++ )
+    {
+      BOUNDINGBOX bb;
+      bb.cipRegion = geometryTopology.GetBoundingBoxChestRegion(i);
+      bb.cipType = geometryTopology.GetBoundingBoxChestType(i);
+
+      for ( unsigned int j=0; j<geometryTopology.GetBoundingBoxStart(i).size(); j++ )
+      	{
+      	  bb.start.push_back(geometryTopology.GetBoundingBoxStart(i)[j]);
+      	}
+      for ( unsigned int j=0; j<geometryTopology.GetBoundingBoxSize(i).size(); j++ )
+      	{
+      	  bb.size.push_back(geometryTopology.GetBoundingBoxSize(i)[j]);
+      	}
+      m_BoundingBoxes.push_back( bb );
+    }
+
+  for ( unsigned int i=0; i<geometryTopology.GetNumberOfPoints(); i++ )
+    {
+      POINT p;
+      p.cipRegion = geometryTopology.GetPointChestRegion(i);
+      p.cipType = geometryTopology.GetPointChestType(i);
+
+      for ( unsigned int j=0; j<geometryTopology.GetPointCoordinate(i).size(); j++ )
+  	{
+  	  p.coordinate.push_back(geometryTopology.GetPointCoordinate(i)[j]);
+  	}
+
+      m_Points.push_back( p );
+    }
+  
+  // Return the existing object
+  return *this;
+}
+
 void cip::GeometryTopologyData::InsertBoundingBox( float start[3], float size[3], 
 						   unsigned char cipRegion = (unsigned char)(cip::UNDEFINEDREGION), 
 						   unsigned char cipType = (unsigned char)(cip::UNDEFINEDTYPE) )
@@ -38,7 +89,7 @@ void cip::GeometryTopologyData::InsertPoint( float coordinate[3],
   this->m_Points.push_back( p );
 }
 
-unsigned char cip::GeometryTopologyData::GetBoundingBoxChestRegion( unsigned int index )
+unsigned char cip::GeometryTopologyData::GetBoundingBoxChestRegion( unsigned int index ) const
 {
   if ( index > this->m_BoundingBoxes.size() )
     {
@@ -50,7 +101,7 @@ unsigned char cip::GeometryTopologyData::GetBoundingBoxChestRegion( unsigned int
   return this->m_BoundingBoxes[index].cipRegion;
 }
 
-unsigned char cip::GeometryTopologyData::GetBoundingBoxChestType( unsigned int index )
+unsigned char cip::GeometryTopologyData::GetBoundingBoxChestType( unsigned int index ) const
 {
   if ( index > this->m_BoundingBoxes.size() )
     {
@@ -62,7 +113,7 @@ unsigned char cip::GeometryTopologyData::GetBoundingBoxChestType( unsigned int i
   return this->m_BoundingBoxes[index].cipType;
 }
 
-unsigned char cip::GeometryTopologyData::GetPointChestRegion( unsigned int index )
+unsigned char cip::GeometryTopologyData::GetPointChestRegion( unsigned int index ) const
 {
   if ( index > this->m_Points.size() )
     {
@@ -74,7 +125,7 @@ unsigned char cip::GeometryTopologyData::GetPointChestRegion( unsigned int index
   return this->m_Points[index].cipRegion;
 }
 
-unsigned char cip::GeometryTopologyData::GetPointChestType( unsigned int index )
+unsigned char cip::GeometryTopologyData::GetPointChestType( unsigned int index ) const
 {
   if ( index > this->m_Points.size() )
     {
@@ -86,7 +137,7 @@ unsigned char cip::GeometryTopologyData::GetPointChestType( unsigned int index )
   return this->m_Points[index].cipType;
 }
 
-cip::GeometryTopologyData::StartType cip::GeometryTopologyData::GetBoundingBoxStart( unsigned int index )
+cip::GeometryTopologyData::StartType cip::GeometryTopologyData::GetBoundingBoxStart( unsigned int index ) const
 {
   if ( index > this->m_BoundingBoxes.size() )
     {
@@ -98,7 +149,7 @@ cip::GeometryTopologyData::StartType cip::GeometryTopologyData::GetBoundingBoxSt
   return this->m_BoundingBoxes[index].start;
 }
 
-cip::GeometryTopologyData::SizeType cip::GeometryTopologyData::GetBoundingBoxSize( unsigned int index )
+cip::GeometryTopologyData::SizeType cip::GeometryTopologyData::GetBoundingBoxSize( unsigned int index ) const
 {
   if ( index > this->m_BoundingBoxes.size() )
     {
@@ -110,7 +161,7 @@ cip::GeometryTopologyData::SizeType cip::GeometryTopologyData::GetBoundingBoxSiz
   return this->m_BoundingBoxes[index].size;
 }
 
-cip::GeometryTopologyData::CoordinateType cip::GeometryTopologyData::GetPointCoordinate( unsigned int index )
+cip::GeometryTopologyData::CoordinateType cip::GeometryTopologyData::GetPointCoordinate( unsigned int index ) const
 {
   if ( index > this->m_Points.size() )
     {
--- Common/cipGeometryTopologyData.h
+++ Common/cipGeometryTopologyData.h
@@ -23,8 +23,8 @@ namespace cip {
   {
   public:
     GeometryTopologyData();
-    GeometryTopologyData( const std::vector< cip::PointType >& );
-    ~GeometryTopologyData() {};
+    GeometryTopologyData& operator= (const GeometryTopologyData &geometryTopology);
+    ~GeometryTopologyData();
     
     typedef std::vector< float > CoordinateType;
     typedef std::vector< float > StartType;
@@ -37,26 +37,26 @@ namespace cip {
     void InsertBoundingBox( float start[3], float size[3], unsigned char cipRegion, 
 			    unsigned char cipType );
 
-    unsigned int GetNumberOfBoundingBoxes()
+    unsigned int GetNumberOfBoundingBoxes() const
     {
       return m_BoundingBoxes.size();
     }
 
     /** Returns the chest-region of the bounding box given the specified index
      *  in the vector of bounding boxes. */
-    unsigned char GetBoundingBoxChestRegion( unsigned int );
+    unsigned char GetBoundingBoxChestRegion( unsigned int ) const;
 
     /** Returns the chest-region of the bounding box given the specified index
      *  in the vector of bounding boxes. */
-    unsigned char GetBoundingBoxChestType( unsigned int );
+    unsigned char GetBoundingBoxChestType( unsigned int ) const;
 
     /** Returns bounding box start location given the specified index
      *  in the vector of bounding boxes. */
-    StartType GetBoundingBoxStart( unsigned int );
+    StartType GetBoundingBoxStart( unsigned int ) const;
 
     /** Returns bounding size given the specified index in the vector of 
      *	bounding boxes. */
-    SizeType GetBoundingBoxSize( unsigned int );
+    SizeType GetBoundingBoxSize( unsigned int ) const;
 
     /** Insert a new point. 'coordinate' is the 3d physical coordinate of the point. 
      *  'cipRegion' and 'cipType' are UNDEFINEDREGION and UNDEFINEDTYPE by default. */
@@ -65,19 +65,19 @@ namespace cip {
     
     /** Returns the chest-region of the point given the specified index
      *  in the vector of point. */
-    unsigned char GetPointChestRegion( unsigned int );
+    unsigned char GetPointChestRegion( unsigned int ) const;
 
     /** Returns the chest-region of the point given the specified index
      *  in the vector of point. */
-    unsigned char GetPointChestType( unsigned int );
+    unsigned char GetPointChestType( unsigned int ) const;
 
-    unsigned int GetNumberOfPoints()
+    unsigned int GetNumberOfPoints() const
     {
       return m_Points.size();
     }
 
     /** Get the spatial coordinate of the point indicated with the specified index */
-    CoordinateType GetPointCoordinate( unsigned int );
+    CoordinateType GetPointCoordinate( unsigned int ) const;
 
   private:
     struct BOUNDINGBOX
