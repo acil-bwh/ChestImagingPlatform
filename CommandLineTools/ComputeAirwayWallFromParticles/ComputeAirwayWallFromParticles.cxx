3cea85807f73a69bdb95aadb2d939eb0c0bd838f
James Ross <jross@bwh.harvard.edu>
2015-05-18 19:34:14 -0400

    ENH: Now using itk-vtk glue instead of NRRD reader

--- CommandLineTools/ComputeAirwayWallFromParticles/ComputeAirwayWallFromParticles.cxx
+++ CommandLineTools/ComputeAirwayWallFromParticles/ComputeAirwayWallFromParticles.cxx
@@ -1,62 +1,80 @@
 #include "cipChestConventions.h"
 #include "vtkSmartPointer.h"
 #include "vtkNRRDReaderCIP.h"
-#include "vtkNRRDWriterCIP.h"
 #include "vtkImageChangeInformation.h"
 #include "vtkPolyDataReader.h"
 #include "vtkPolyDataWriter.h"
 #include "vtkImageMathematics.h"
 #include "vtkComputeAirwayWall.h"
 #include "vtkComputeAirwayWallPolyData.h"
+#include "itkImageToVTKImageFilter.h"
 #include <vtksys/SystemTools.hxx>
+#include "cipHelper.h"
 
 #include "ComputeAirwayWallFromParticlesCLP.h"
+
+namespace
+{
+  typedef itk::ImageToVTKImageFilter< cip::CTType > ConnectorType;
+}
 
 int main( int argc, char *argv[] )
 {
   PARSE_ARGS;
   
-  //Reading data
   std::cout << "Reading CT..." << std::endl;
-  vtkSmartPointer< vtkNRRDReaderCIP > ctReader = vtkSmartPointer< vtkNRRDReaderCIP >::New();
-    ctReader->SetFileName( inCT.c_str() );
-    ctReader->SetUseNativeOriginOn();
-    ctReader->Update();
-  
-  vtkMatrix4x4 *ras = ctReader->GetRasToIjkMatrix();
+  cip::CTReaderType::Pointer reader = cip::CTReaderType::New();
+    reader->SetFileName( inCT );
+  try
+    {
+    reader->Update();
+    }
+  catch ( itk::ExceptionObject &excp )
+    {
+    std::cerr << "Exception caught reading mask:";
+    std::cerr << excp << std::endl;
+
+    return cip::NRRDREADFAILURE;
+    }
+
+  ConnectorType::Pointer connector = ConnectorType::New();
+    connector->SetInput( reader->GetOutput() );
+    connector->Update();
+
+  // Note: since we converted to using the ITK-VTK glue to handle reading the image,
+  // we are now using the 'connector' to communicate with VTK. 'ras' here no longer
+  // serves the purpose it previously did.
+  vtkSmartPointer< vtkMatrix4x4 > ras = vtkSmartPointer< vtkMatrix4x4 >::New();
   
   //Particles are in LPS (b/c we assume that images come from DICOM).
   //NRRD reader assumes RAS, so we have to transform the origin for the pipeline to work
   vtkSmartPointer< vtkImageChangeInformation > ctOrigin = vtkSmartPointer< vtkImageChangeInformation >::New();
-  ctOrigin->SetInputData(ctReader->GetOutput());
-  ctOrigin->SetOutputOrigin(-1.0*ras->GetElement(0,3),-1.0*ras->GetElement(1,3),ras->GetElement(2,3));
-  ctOrigin->Update();
+    ctOrigin->SetInputData( connector->GetOutput() );
+    ctOrigin->SetOutputOrigin(-1.0*ras->GetElement(0,3),-1.0*ras->GetElement(1,3),ras->GetElement(2,3));
+    ctOrigin->Update();
   
   std::cout << "Reading particles..." << std::endl;
   vtkSmartPointer< vtkPolyDataReader > airwayParticlesReader = vtkSmartPointer< vtkPolyDataReader >::New();
-  airwayParticlesReader->SetFileName( inAirwayParticles.c_str() );
-  airwayParticlesReader->Update();
+    airwayParticlesReader->SetFileName( inAirwayParticles.c_str() );
+    airwayParticlesReader->Update();
 
   vtkPolyData* particles=airwayParticlesReader->GetOutput();
   
   //Transform CT data to be positive
   vtkSmartPointer< vtkImageMathematics > transformedCT = vtkSmartPointer< vtkImageMathematics >::New();
-  transformedCT->SetInput1Data(ctOrigin->GetOutput());
-  transformedCT->SetOperationToAddConstant();
-  transformedCT->SetConstantC(1024);
-  transformedCT->Update();
-    
-  vtkSmartPointer< vtkNRRDWriterCIP > ww= vtkSmartPointer< vtkNRRDWriterCIP >::New();
-  ww->SetFileName( "test-wall.nrrd" );
-  ww->SetInputData(transformedCT->GetOutput());
-  ww->Update();
+    transformedCT->SetInput1Data(ctOrigin->GetOutput());
+    transformedCT->SetOperationToAddConstant();
+    transformedCT->SetConstantC(1024);
+    transformedCT->Update();
   
   //Check to see if we have the proper point data field
   if (inPlane == false && particles->GetPointData()->GetArray("hevec2") == NULL)
     {
       std::cerr<<"Particle data does not have local frame information. This vtk file might not be a particle dataset"<<std::endl;
       return cip::EXITFAILURE;
-    } else {
+    } 
+  else 
+    {
       particles->GetPointData()->SetActiveVectors("hevec2");
     }
   
@@ -64,23 +82,26 @@ int main( int argc, char *argv[] )
   vtkSmartPointer< vtkComputeAirwayWall > solver  = vtkSmartPointer< vtkComputeAirwayWall >::New();
   
   if (method == "FWHM")
-  {
-    solver->SetMethod(0);
-  } else if (method == "ZC")
-  {
-    solver->SetMethod(1);
-  } else if (method == "PC")
-  {
-    solver->SetMethod(2);
-  }
+    {
+      solver->SetMethod(0);
+    } 
+  else if (method == "ZC")
+    {
+      solver->SetMethod(1);
+    } 
+  else if (method == "PC")
+    {
+      solver->SetMethod(2);
+    }
   
   solver->SetNumberOfThetaSamples(numberOfRays);
 
   // Airway wall computing filter
-  vtkSmartPointer< vtkComputeAirwayWallPolyData > filter  = vtkSmartPointer< vtkComputeAirwayWallPolyData >::New();
-  filter->SetInputData(particles);
-  filter->SetImage(transformedCT->GetOutput());
-  filter->SetWallSolver(solver);
+  vtkSmartPointer< vtkComputeAirwayWallPolyData > filter = 
+    vtkSmartPointer< vtkComputeAirwayWallPolyData >::New();
+    filter->SetInputData(particles);
+    filter->SetImage(transformedCT->GetOutput());
+    filter->SetWallSolver(solver);
   
   // Setting up thresholds
   solver->SetGradientThreshold(gradientThreshold);
@@ -88,19 +109,20 @@ int main( int argc, char *argv[] )
   solver->SetPCThreshold(pcThreshold);
   
   if ( largeAirways == true)
-  {
-    solver->SetRMax(25);
-    filter->SetResolution(0.2);
-  }
-  
-  
+    {
+      solver->SetRMax(25);
+      filter->SetResolution(0.2);
+    }
+    
   if (inPlane == true)
-  {
-    filter->ReformatOff();
-  } else {
-    filter->ReformatOn();
-    filter->SetAxisModeToVector();
-  }
+    {
+      filter->ReformatOff();
+    } 
+  else 
+    {
+      filter->ReformatOn();
+      filter->SetAxisModeToVector();
+    }
       
   if (saveAirwayImages == true)
     {
@@ -109,14 +131,14 @@ int main( int argc, char *argv[] )
       components.push_back(outputDirectory + "/");
       components.push_back(saveAirwayPrefix);
       filter->SetAirwayImagePrefix(vtksys::SystemTools::JoinPath(components).c_str());
-    } else
+    } 
+  else
     {
       filter->SaveAirwayImageOff();
     }
     
   filter->Update();
   
-
   std::cout << "Writing particles file..." << std::endl;
   vtkSmartPointer< vtkPolyDataWriter > particlesWriter = vtkSmartPointer< vtkPolyDataWriter >::New();
     particlesWriter->SetFileName( outAirwayParticles.c_str() );
