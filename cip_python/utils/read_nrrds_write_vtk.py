b687a97076ac243804d4e80ce72925a9309efb59
Raul San Jose <rjosest@gmail.com>
2015-05-31 20:19:18 -0400

    ENH: new methods to add metainformation to the particles

--- cip_python/utils/read_nrrds_write_vtk.py
+++ cip_python/utils/read_nrrds_write_vtk.py
@@ -9,13 +9,24 @@ class ReadNRRDsWriteVTK:
     out_file_name : string
         Name of output VTK file
     """
-    def __init__(self, out_file_name):
+    def __init__(self, out_file_name,binary=True):
         self._argumentList = ""
         self._argumentList = self._argumentList + " -o " + out_file_name
+        if binary == True:
+            self._argumentList = self._argumentList + " -b"
 
     def add_file_name_array_name_pair( self, fileName, arrayName ):
         self._argumentList = self._argumentList + " -i " + fileName + " -a " + arrayName
+    
+    def add_metadata_name_value_pair( self, name, value):
+
+        if type(value) == type(tuple()) or type(value) == type(list()):
+            value=",".join([str(val) for val in value])
+        
+        self._argumentList = self._argumentList + " --%(name)s %(value)s" % \
+                            {'name':name,'value':value}
 
     def execute( self ):
+        print self._argumentList
         tmpCommand = "ReadNRRDsWriteVTK " + self._argumentList
         subprocess.call( tmpCommand, shell=True )
