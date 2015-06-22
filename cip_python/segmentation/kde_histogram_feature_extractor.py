7b8fdf598de1aa16d7ea8b5b727b453f3ca63744
James Ross <jross@bwh.harvard.edu>
2015-06-22 12:58:32 -0400

    ENH: Numerous style changes

--- cip_python/segmentation/kde_histogram_feature_extractor.py
+++ cip_python/segmentation/kde_histogram_feature_extractor.py
@@ -6,6 +6,7 @@ from sklearn.grid_search import GridSearchCV
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
+import nrrd
 import pdb
 from kde_bandwidth import botev_bandwidth
 import pandas as pd
@@ -13,7 +14,7 @@ import pandas as pd
      
 class kdeHistExtractor:
     """General purpose class implementing a kernel density estimation histogram
-        extractor. 
+    extractor. 
 
     The user inputs CT numpy array, a mask, and a patch segmentation. The
     output is a pandas dataframe with patch #=number and
@@ -25,111 +26,92 @@ class kdeHistExtractor:
         Number of bins to be produced
     
     lower_limit:int
-        lower limit of histogram
+        Lower limit of histogram
         
     upper_limit:int
-        upper limit of histogram
-    
-    in_patches: 3D numpy array, shape (L, M, N)
-        Input patch labelmap
-        
-    in_ct: 3D numpy array, shape (L, M, N)
-        Input CT image to be segmentaed
-    
-    in_lm: 3D numpy array, shape (L, M, N)
-        Input mask where histograms will be computed.         
-        
+        Upper limit of histogram
 
-    Returns
-    --------
-    kde_histogram: pandas dataframe
-        
+    Attribues
+    ---------
+    df_ : Pandas dataframe
+        Contains the computed histogram information. The 'patch_label' column
+        corresponds to the segmented patch over which the histogram is 
+        computed. The remaining columns are named 'huNum', where 'Num' ranges 
+        from 'lower_limit' to 'upper_limit'. These columns record the number of 
+        counts for each Hounsfield unit (hu) estimated by the KDE.        
     """
-
-
-
-    def __perform_kde_botev__(self, input_data):
-        """
-        perform kernel density estimation  (using botev estimatir for best bandwidth) 
+    def __init__(self, lower_limit=-1050, upper_limit=3050, num_bins= 4096):        
+        # Initialize the dataframe       
+        # first get the list of all histogaram bin values
+        cols = ['patch_label']
+        self.bin_values = np.linspace(lower_limit, upper_limit, num_bins)
+        
+        for i in self.bin_values:
+            cols.append('hu' + str(i))
+            
+        self.df_ = pd.DataFrame(columns=cols)
+    
+    def _perform_kde_botev(self, input_data):
+        """Perform kernel density estimation  (using botev estimator for best 
+        bandwidth) 
     
         Parameters 
         ----------
-        input_data: 1D numpy array
+        input_data : 1D numpy array
             Array intensity values for which to perform kde
         
         Returns
         --------
-        kde_histogram: pandas dataframe
-            
-        """
-        
+        hist : array, shape (n_bins)
+            The computed histogram
+        """        
         input_data = np.squeeze(input_data)
 
         kde_bandwidth_estimator = botev_bandwidth()
         the_bandwidth = kde_bandwidth_estimator.run(input_data)
         kde = KernelDensity(bandwidth=the_bandwidth)
         kde.fit(input_data[:, np.newaxis])
-
         
-        """ get histogram  (Raul limit to 4096 samples from -1050 till 3050) """
+        # Get histogram 
         X_plot = self.bin_values[:, np.newaxis]
         log_dens = kde.score_samples(X_plot)
-        dens = np.exp(log_dens)
-        print(np.sum(dens))
-        fig, ax = plt.subplots(1, 1, sharex=True, sharey=True)
-        ax.fill(self.bin_values, np.exp(log_dens), fc='#AAAAFF')
-        plt.show()       
-        return dens        
-                        
-    def __init__(self, lower_limit=-1050, upper_limit=3050, num_bins= 4096):        
-        # initialize the dataframe       
-        # first get the list of all histogaram bin values
-        cols = ['patch_label']
-        self.bin_values = np.linspace(lower_limit, upper_limit, num_bins)
+        hist = np.exp(log_dens)
+
+        return hist
         
-        for i in self.bin_values:
-            cols.append(str(i))
-            
-        self._df = pd.DataFrame(columns=cols)
-        
-        
-    def execute(self, ct, lm, patch_labels):
-        """ compute the histogram
+    def fit(self, ct, lm, patch_labels):
+        """Compute the histogram of each patch defined in 'patch_labels' beneath
+        non-zero voxels in 'lm' using the CT data in 'ct'.
         
         Parameters
         ----------
         patch_labels: 3D numpy array, shape (L, M, N)
-            Input patch labelmap
+            Input patch segmentation
         
         ct: 3D numpy array, shape (L, M, N)
-            Input CT image to be segmentaed
+            Input CT image from which histogram information will be derived
     
         lm: 3D numpy array, shape (L, M, N)
             Input mask where histograms will be computed.    
-        """
-        
+        """        
         unique_patch_labels = np.unique(patch_labels)
-        """ loop through each patch """
+        # loop through each patch 
         for p_label in unique_patch_labels:               
-                print("PATCH LABEL "+str(p_label))
-                """ extract the lung area from the CT for the patch"""
-                patch_intensities = ct[np.logical_and(patch_labels==p_label, lm >0)] 
+            # extract the lung area from the CT for the patch
+            patch_intensities = ct[np.logical_and(patch_labels==p_label, lm >0)] 
         
-                """ linearize features"""
-                intensity_vector = np.array(patch_intensities.ravel()).T
-                if (np.shape(intensity_vector)[0] > 1):
-                    print(np.shape(intensity_vector)[0] )
-                    """ obtain kde histogram from features"""
-                    histogram = self.__perform_kde_botev__(intensity_vector)
-                    tmp = dict()
-                    tmp['patch_label'] = p_label
-                    for i in range(0,np.shape(self.bin_values)[0]):
-                        tmp[self.bin_values[i]]= histogram[i]
-                    """ save in data frame """
-                    self._df = self._df.append(tmp, ignore_index=True)
-        return self._df
+            # linearize features
+            intensity_vector = np.array(patch_intensities.ravel()).T
+            if (np.shape(intensity_vector)[0] > 1):
+                # obtain kde histogram from features
+                histogram = self._perform_kde_botev(intensity_vector)
+                tmp = dict()
+                tmp['patch_label'] = p_label
+                for i in range(0,np.shape(self.bin_values)[0]):
+                    tmp[self.bin_values[i]]= histogram[i]
+                # save in data frame 
+                self.df_ = self.df_.append(tmp, ignore_index=True)
         
-
 if __name__ == "__main__":
     desc = """Generates histogram features given input CT and segmentation \
     data"""
@@ -162,16 +144,14 @@ if __name__ == "__main__":
     (options, args) = parser.parse_args()
     
     #image_io = ImageReaderWriter()
-    import nrrd
-    ct,ct_header=  nrrd.read(options.in_ct) #image_io.read_in_numpy(options.in_ct)
-    lm,lm_header=  nrrd.read(options.in_lm) 
-    in_patches,in_patches_header=  nrrd.read(options.in_patches) 
+    ct,ct_header = nrrd.read(options.in_ct) #image_io.read_in_numpy(options.in_ct)
+    lm,lm_header = nrrd.read(options.in_lm) 
+    in_patches,in_patches_header = nrrd.read(options.in_patches) 
     
     kde_hist_extractor = kdeHistExtractor(lower_limit=options.lower_limit, \
-        upper_limit=options.upper_limit, num_bins=options.num_bins)
-                 
-    kde_hist_datafram = kde_hist_extractor.execute(ct, lm, in_patches)
+        upper_limit=options.upper_limit, num_bins=options.num_bins)                 
+    kde_hist_extractor.fit(ct, lm, in_patches)
 
     if options.out_csv is not None:
-        kde_hist_datafram.to_csv(options.out_csv, index=False)
+        kde_hist_datafram.df_.to_csv(options.out_csv, index=False)
         
