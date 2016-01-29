89934c4f267e0c4a9184d73c8d1aa7564ac67ac7
James Ross <jross@bwh.harvard.edu>
2016-01-29 17:48:30 -0500

    Exposing number of iterations. Also removing option to perform scale space particles -- the algorithm will be exeucted for a single scale. Also adding comments

--- cip_python/particles/fissure_particles.py
+++ cip_python/particles/fissure_particles.py
@@ -23,26 +23,14 @@ class FissureParticles(ChestParticles):
     mask_file_name : string (optional)
         File name of mask within which to execute particles
 
-    max_scale : float (optional)
-        The maximum scale to consider in scale space (default is 6.0). If
-        larger structures are desired, it's advised to downsample the input
-        image using the 'down_sample_rate' parameter and not to simply increase
-        'max_scale'. For example, to capture a structure that is best
-        represented at a scale of 12, keep 'max_scale' at 6 and downsample by
-        2. The scale of the output particles is handled properly.    
+    scale : float (optional)
+        The scale of the fissure to consider in scale space.
 
     live_thresh : float (optional)
-
+        Threshold to use when pruning particles during population control.
+    
     seed_thresh : float (optional)
-
-    scale_samples : int (optional)
-        The number of pre-blurrings performed on the input image. These
-        pre-blurrings are saved to the specified temp directory and used for
-        interpolation across scale. The scale at which a given blurring is
-        performed is also a function of the 'max_scale' parameter. Note that
-        blurrings are not performed uniformly on the interval [0, max_scale].
-        Instead, more blurrings are performed at the low end in order to better
-        capture smaller structures. Default value is 1.
+        Threshold to use when initializing particles.
 
     down_sample_rate : int (optional)
         The amount by which the input image will be downsampled prior to
@@ -60,22 +48,22 @@ class FissureParticles(ChestParticles):
         The number of iterations for which to run the algorithm.
     """
     def __init__(self, in_file_name, out_particles_file_name, tmp_dir,
-                 mask_file_name=None, max_scale=1.2, live_thresh=-30.6,
-                 seed_thresh=-30.6, scale_samples=1, down_sample_rate=1,
-                 min_intensity=-1000, max_intensity=-500, iterations=200):
+                 mask_file_name=None, scale=0.9, live_thresh=-15,
+                 seed_thresh=-45, down_sample_rate=1,
+                 min_intensity=-920, max_intensity=-400, iterations=200):
         ChestParticles.__init__(self, feature_type="ridge_surface",
             in_file_name=in_file_name, 
             out_particles_file_name=out_particles_file_name,
             tmp_dir=tmp_dir, mask_file_name=mask_file_name,
-            max_scale=max_scale, scale_samples=scale_samples,
+            max_scale=scale, scale_samples=1,
             down_sample_rate=down_sample_rate)
 
         self._max_intensity = max_intensity
         self._min_intensity = min_intensity
         self._live_thresh = live_thresh
         self._seed_thresh = seed_thresh
-        self._max_scale = max_scale
-        self._scale_samples = scale_samples
+        self._max_scale = scale
+        self._scale_samples = 1
         self._down_sample_rate = down_sample_rate
         self._iterations = iterations
         
@@ -111,93 +99,23 @@ class FissureParticles(ChestParticles):
         # Temporary nrrd particles points
         out_particles = os.path.join(self._tmp_dir, "pass%d.nrrd")
 
-        if self._scale_samples == 1:
-            self._single_scale = 1
-            #Setting up single scale approach
-            self._use_strength = False
-            self._inter_particle_energy_type = "justr"
-            self._init_mode = "Random"
-            self._number_init_particles = 12000
-
-            self._beta  = 0 # Irrelevant for pass 1
-            self._alpha = 0.5
-            self._irad = 1.7 
-            self._srad = 1.2
+        self._single_scale = 1
+        #Setting up single scale approach
+        self._use_strength = False
+        self._inter_particle_energy_type = "justr"
+        self._init_mode = "Random"
+        self._number_init_particles = 12000
+        
+        self._beta  = 0 # Irrelevant for pass 1
+        self._alpha = 0.5
+        self._irad = 1.7 
+        self._srad = 1.2
             
-            #Build parameters and run
-            self.reset_params()
-            self.build_params()
+        #Build parameters and run
+        self.reset_params()
+        self.build_params()
 
-            self.execute_pass(out_particles % 3)
-
-        else:        
-            #Pass 1
-            #Init params
-            self._use_strength = False
-            self._inter_particle_energy_type = "uni"
-            self._init_mode = "Random"
-            self._number_init_particles = 12000
-
-            # Energy
-            # Radial energy function (psi_1 in the paper)
-            self._inter_particle_enery_type = "uni"
-            self._beta  = 0.7 # Irrelevant for pass 1
-            self._alpha = 0.5
-            self._irad = 4
-            self._srad = 1.2
-            self._iterations = 10
-
-            #Build parameters and run
-            self.reset_params()
-            self.build_params()
-            self.execute_pass(out_particles % 1)
-
-            # Pass 2
-            # Init params
-            self._init_mode = "Particles"
-            self._in_particles_file_name = out_particles % 1
-            self._use_mask = False #TODO: was 0
-
-            # Energy
-            # Radial energy function (psi_2 in the paper).
-            # Addition of 2 components: scale and space
-            self._inter_particle_energy_type = "add"
-            self._alpha = 0
-
-            # Controls blending in scale and space with respect to
-            # function psi_2
-            self._beta = 0.5
-            self._irad = 4
-            self._srad = 2
-            self._use_strength = True
-
-            self._iterations = 20
-
-            # Build parameters and run
-            self.reset_params()
-            self.build_params()
-            self.execute_pass(out_particles % 2)
-
-            # Pass 3
-            self._init_mode = "Particles"
-            self._in_particles_file_name = out_particles % 2
-            self._use_mask = False # TODO: was 0
-
-            # Energy
-            self._inter_particle_energy_type = "add"
-            self._alpha = 0.25
-            self._beta = 0.25
-            self._gamma = 0.002
-            self._irad = 4
-            self._srad = 4
-            self._use_strength = True
-            self._use_mode_th = True
-            self._iterations = 50
-
-            # Build parameters and run
-            self.reset_params()
-            self.build_params()
-            self.execute_pass(out_particles % 3)
+        self.execute_pass(out_particles % 3)
 
         # Probe quantities and save to VTK
         self.probe_quantities(self._tmp_in_file_name, out_particles % 3)
@@ -212,49 +130,41 @@ class FissureParticles(ChestParticles):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-i", help='input CT scan', dest="input_ct")
-    parser.add_argument("-m", help='input mask for seeding', dest="input_mask")
-    parser.add_argument("-o", help='output particles (vtk format)', \
-                      dest="output_particles")
-    parser.add_argument("-t", help='tmp directory', dest="tmp_dir", 
-                        default=None)
-    parser.add_argument("-s", help='max scale (default 1.2)', 
-                        dest="max_scale", default=1.2)
-    parser.add_argument("-r", help='down sampling rate. Must be greater than \
+    parser.add_argument("-i", help='Input CT scan', dest="input_ct")
+    parser.add_argument("-m", help='Input mask for seeding', dest="input_mask")
+    parser.add_argument("-o", help='Output particles (vtk format)', \
+      dest="output_particles")
+    parser.add_argument("-t", help='Temp directory in which to store \
+      intermediate files', dest="tmp_dir", default=None)
+    parser.add_argument("-s", help='The scale of the fissure to consider in \
+      scale space.', dest="scale", default=0.9)
+    parser.add_argument("-r", help='Down sampling rate. Must be greater than \
       or equal to 1.0 (default 1.0)', dest="down_sample_rate", default=1.0)
-    parser.add_argument("-n", help='number of scale volumes (default 1)', \
-                      dest="scale_samples", default=1)
-    parser.add_argument("--lth", help='live threshold. Must be less than zero \
-      (default -30)', dest="live_th", default=-30)
-    parser.add_argument("--sth", help='seed threshold. Must be less than zero \
-      (defaut -30)', dest="seed_th", default=-30)
-    parser.add_argument("--minI", 
-                        help='min intensity for feature (default -1000)',
-                        dest="min_intensity", default=-1000)
-    parser.add_argument("--maxI", 
-                        help='max intensity for feature (default -500)',
-                        dest="max_intensity", default=-500)
-    parser.add_argument("--iters", 
-                        help='Number of algorithm iterations (default 200)',
-                        dest="iters", default=200)    
+    parser.add_argument("--lth", help='Threshold to use when pruning particles \
+      during population control. Must be less than zero', 
+      dest="live_th", default=-15)
+    parser.add_argument("--sth", help='Threshold to use when initializing \
+      particles. Must be less than zero', dest="seed_th", default=-45)
+    parser.add_argument("--minI", help='Histogram equilization will be applied \
+      to enhance the fissure features within the range [minI, maxI]', 
+      dest="min_intensity", default=-920)
+    parser.add_argument("--maxI", help='Histogram equilization will be applied \
+      to enhance the fissure features within the range [minI, maxI]', 
+      dest="max_intensity", default=-400)
+    parser.add_argument("--iters", help='Number of algorithm iterations \
+      (default 200)', dest="iters", default=200)    
     
     op = parser.parse_args()
     
-    #Max scale has to be an integer for multi-scale particles
-    if int(op.scale_samples) > 1:
-        max_scale = round(float(op.max_scale))
-    else:
-        max_scale = float(op.max_scale)
-
     if op.tmp_dir is not None:
         tmp_dir = op.tmp_dir
     else:        
         tmp_dir = tempfile.mkdtemp()
 
     dp = FissureParticles(op.input_ct, op.output_particles, tmp_dir, 
-        op.input_mask, max_scale, float(op.live_th), float(op.seed_th), 
-        int(op.scale_samples), float(op.down_sample_rate), 
-        float(op.min_intensity), float(op.max_intensity), int(op.iters))
+        op.input_mask, float(op.scale), float(op.live_th), float(op.seed_th), 
+        float(op.down_sample_rate), float(op.min_intensity), 
+        float(op.max_intensity), int(op.iters))
     dp.execute()
     
     if op.tmp_dir is None:
