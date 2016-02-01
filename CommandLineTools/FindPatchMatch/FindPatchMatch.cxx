7a6f73298bc7e2bc528dce30d2dfa177cd201e94
James Ross <jross@bwh.harvard.edu>
2016-02-01 11:34:34 -0500

    Misc changes

--- CommandLineTools/FindPatchMatch/FindPatchMatch.cxx
+++ CommandLineTools/FindPatchMatch/FindPatchMatch.cxx
@@ -41,48 +41,83 @@ int main( int argc, char *argv[] )
     std::cerr << excp << std::endl;
     }
 
-  cip::CTType::IndexType patchStart;
+  cip::CTType::IndexType localStart;
   cip::CTType::SizeType patchSize;
     patchSize[0] = patchReader->GetOutput()->GetBufferedRegion().GetSize()[0];
     patchSize[1] = patchReader->GetOutput()->GetBufferedRegion().GetSize()[1];
     patchSize[2] = patchReader->GetOutput()->GetBufferedRegion().GetSize()[2];
 
-  cip::CTType::RegionType patchRegion;
-  patchRegion.SetSize( patchSize );
-  
+  cip::CTType::IndexType index1;
+  cip::CTType::IndexType index2;
+  int patchSum = 0;
+  short patchMax = -2000;
+  for ( unsigned int i=0; i<patchSize[0]; i++ )
+    {
+      for ( unsigned int j=0; j<patchSize[1]; j++ )
+	{
+	  index1[0] = i;
+	  index1[1] = j;
+	  index1[2] = 0;
+	  short val = patchReader->GetOutput()->GetPixel( index1 );
+	  patchSum += val;
+	  if ( val > patchMax )
+	    {
+	      patchMax = val;
+	    }
+	}
+    }
+
+  cip::CTType::RegionType localRegion;
+    localRegion.SetSize( patchSize );
+
   bool foundPatch = false;
 
   IteratorType ctIt( ctReader->GetOutput(), ctReader->GetOutput()->GetBufferedRegion() );
+  IteratorType pIt( patchReader->GetOutput(), patchReader->GetOutput()->GetBufferedRegion() );
 
   std::cout << "Searching for patch matches..." << std::endl;
   ctIt.GoToBegin();
   while ( !ctIt.IsAtEnd() )
     {
-      patchStart[0] = ctIt.GetIndex()[0];
-      patchStart[1] = ctIt.GetIndex()[1];
-      patchStart[2] = ctIt.GetIndex()[2];
+      localStart[0] = ctIt.GetIndex()[0];
+      localStart[1] = ctIt.GetIndex()[1];
+      localStart[2] = ctIt.GetIndex()[2];
 
-      patchRegion.SetIndex( patchStart );
+      if ( localStart[0] + patchSize[0] <= ctReader->GetOutput()->GetBufferedRegion().GetSize()[0] &&
+      	   localStart[1] + patchSize[1] <= ctReader->GetOutput()->GetBufferedRegion().GetSize()[1] &&
+      	   localStart[2] + patchSize[2] <= ctReader->GetOutput()->GetBufferedRegion().GetSize()[2] )
+      	{
+      	  localRegion.SetIndex( localStart );
+	  IteratorType localIt( ctReader->GetOutput(), localRegion );
 
-      IteratorType pIt( patchReader->GetOutput(), patchRegion );
-
-      foundPatch = true;      
-      pIt.GoToBegin();
-      while ( !pIt.IsAtEnd() )
-	{
-	  if ( ctReader->GetOutput()->GetPixel( ctIt.GetIndex() ) != pIt.Get() )
+	  int localSum = 0;
+	  short localMax = -2000;
+      	  foundPatch = true;      
+      	  pIt.GoToBegin();
+	  localIt.GoToBegin();
+      	  while ( !pIt.IsAtEnd() )
 	    {
-	      foundPatch = false;
-	      break;
-	    }
+	      localSum += localIt.Get();
+	      if ( localIt.Get() > localMax )
+		{
+		  localMax = localIt.Get();
+		}
+	      // if ( localIt.Get() != pIt.Get() )
+      	      // 	{
+      	      // 	  foundPatch = false;	
+      	      // 	  break;
+      	      // 	}
 
-	  ++pIt;
-	}
-      
-      if ( foundPatch )
-	{
-	  std::cout << patchStart << std::endl;
-	}
+      	      ++pIt;
+	      ++localIt;
+      	    }
+
+      	  //if ( foundPatch )
+      	  if ( localSum == patchSum && localMax == patchMax )
+      	    {
+      	      std::cout << localStart << std::endl;
+      	    }
+      	}
 
       ++ctIt;
     }
