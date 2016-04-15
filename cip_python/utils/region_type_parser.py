7488abc78a959b5a1349568a6e12990428bf11e5
James Ross <jross@bwh.harvard.edu>
2016-04-15 11:55:20 -0400

    STYLE: minor style changes

--- cip_python/utils/region_type_parser.py
+++ cip_python/utils/region_type_parser.py
@@ -1,15 +1,10 @@
 import numpy as np
 from cip_python.ChestConventions import ChestConventions
-import time
 from sets import Set
 import pdb
 
-def f(n, _set): return n in _set
-
 def contains(label, set_of_labels):
-        """ True if label is in a set of values. 
-        That means: if label is in set_of_labels, the function returns True.
-        I will leave to the reader the deduction of what the function returns if label is not in set_of_labels..
+        """ True if label is in the set of labels. 
         """
         return label in set_of_labels
         
@@ -36,9 +31,6 @@ class RegionTypeParser():
 
         self.labels_ = np.unique(self._data)
    
-
-    
-
     def get_mask(self, chest_region=None, chest_type=None):
         """Get's boolean mask of all data indices that match the chest-region
         chest-type query.
@@ -101,15 +93,6 @@ class RegionTypeParser():
         
         contains_np = np.frompyfunc(contains, 2, 1)
         mask = contains_np(self._data, Set(mask_labels)).astype(bool)
-
-        ### This below here is the bottleneck
-        #print(mask_labels)
-        #for ml in mask_labels:
-        #    print(ml)
-        #    #mask = np.logical_or(mask, self._data == ml)
-        #    mask[self._data == ml] = 1 #np.logical_or(mask, self._data == ml)
-        #    toc0 = time.clock()                    
-        #    print("time in mask labels loop new"+ str(toc0-tic))
 
         return mask
 
