32f061bfc458f4986fcd897d6afc581a757300b7
Raul San Jose <rjosest@gmail.com>
2017-01-21 18:58:02 -0500

    ENH: Adding different resampling options. Small modifications

--- Scripts/cip_compute_vessel_particles.py
+++ Scripts/cip_compute_vessel_particles.py
@@ -5,9 +5,12 @@ import string
 import subprocess
 import os
 import nrrd
+import SimpleITK as sitk
 
 from cip_python.particles.vessel_particles import VesselParticles
 from cip_python.particles.multires_vessel_particles import MultiResVesselParticles
+from cip_python.utils.interpolate_volume import InterpolateVolume
+
 
 class VesselParticlesPipeline:
     """Class that implements a vessel particle extraction pipeline for a given region
@@ -16,8 +19,8 @@ class VesselParticlesPipeline:
         ---------
         
         """
-    def __init__(self,ct_file_name,pl_file_name,regions,tmp_dir,output_prefix,init_method='Frangi',\
-                 lth=-95,sth=-70,voxel_size=0,min_scale=0.7,max_scale=4,crop=0,rate=1,multires=False,justparticles=False,clean_cache=True):
+    def __init__(self,ct_file_name,pl_file_name,regions,tmp_dir,output_prefix,init_method='Frangi',resampling_method='Linear',\
+                 lth=-95,sth=-70,voxel_size=0,min_scale=0.7,max_scale=4,vesselness_th=0.38,crop=0,rate=1,multires=False,justparticles=False,clean_cache=True):
         
         assert init_method == 'Frangi' or init_method == 'Threshold' or init_method == 'StrainEnergy'
         
@@ -27,6 +30,7 @@ class VesselParticlesPipeline:
         self._tmp_dir=tmp_dir
         self._output_prefix=output_prefix
         self._init_method=init_method
+        self._resampling_method=resampling_method
         self._lth=lth
         self._sth=sth
         self._voxel_size=voxel_size
@@ -44,7 +48,7 @@ class VesselParticlesPipeline:
         #Distance from wall that we don't want to consider in the initialization (negative= inside the lung, positive= outside the lung)
         self._distance_from_wall = -2
         #Threshold on the vesselness map (for particles initialization mask)
-        self._vesselness_th = 0.38
+        self._vesselness_th = vesselness_th
         #Intensity threshold (for particles initialization mask)
         self._intensity_th = -700
     
@@ -72,13 +76,14 @@ class VesselParticlesPipeline:
         if self._justparticles == False:
             
             #Crop volume if flag is set
-            ct_file_name_crop = os.path.join(self._tmp_dir,self._case_id + "_crop.nhdr")
-            pl_file_name_crop = os.path.join(self._tmp_dir,self._case_id + "_croppartialLungLabelMap.nhdr")
+            ct_file_name_crop = os.path.join(self._tmp_dir,self._case_id + "_crop.nrrd")
+            pl_file_name_crop = os.path.join(self._tmp_dir,self._case_id + "_croppartialLungLabelMap.nrrd")
             if crop_flag == True:
                 if self._rate == 1:
                     ratestr = "="
                 else:
                     ratestr = 'x%f' % rate
+                
                 tmpCommand = "unu crop -min 0 0 %(z1)d -max M M %(z2)d -i %(in)s | unu resample -k %(kernel)s -s = = %(ratestr)s -o %(out)s"
                 
                 tmpCommandCT = tmpCommand % {'z1':crop[0],'z2':crop[1],'in':ct_file_name,'out':ct_file_name_crop,'ratestr':ratestr,'kernel':"cubic:1,0"}
@@ -92,33 +97,62 @@ class VesselParticlesPipeline:
                 pl_file_name = pl_file_name_crop
             
             #Do resampling to isotropic voxel size to compute accurate scale measurements
-            ct_file_name_resample = os.path.join(self._tmp_dir,self._case_id + "_resample.nhdr")
-            pl_file_name_resample = os.path.join(self._tmp_dir,self._case_id + "_resamplepartialLungLabelMap.nhdr")
+            ct_file_name_resample = os.path.join(self._tmp_dir,self._case_id + "_resample.nrrd")
+            pl_file_name_resample = os.path.join(self._tmp_dir,self._case_id + "_resamplepartialLungLabelMap.nrrd")
             
             if self._voxel_size>0:
-                tmpCommand = "unu resample -k %(kernel)s -s x%(f1)f x%(f2)f x%(f3)f -i %(in)s -o %(out)s"
-                tmpCommandCT = tmpCommand % {'in':ct_file_name,'out':ct_file_name_resample,'kernel':"tent",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':spacing[2]/self._voxel_size}
-                tmpCommandPL = tmpCommand % {'in':pl_file_name,'out':pl_file_name_resample,'kernel':"cheap",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':spacing[2]/self._voxel_size}
-                print tmpCommandCT
-                print tmpCommandPL
                 
-                subprocess.call(tmpCommandCT,shell=True)
+                tmpCommandUnuResampling = "unu resample -k %(kernel)s -s x%(f1)f x%(f2)f x%(f3)f -i %(in)s -o %(out)s -c cell"
+
+                #CT Resampling: Three approches: Linear, cubic, registration and hybrid (registration and cubic)
+                if self._resampling_method=='Linear':
+                    tmpCommandCT = tmpCommandUnuResampling % {'in':ct_file_name,'out':ct_file_name_resample,'kernel':"tent",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':spacing[2]/self._voxel_size}
+                    print tmpCommandCT
+                    subprocess.call(tmpCommandCT,shell=True)
+                elif self._resampling_method=='Cubic':
+                    tmpCommandCT = tmpCommandUnuResampling % {'in':ct_file_name,'out':ct_file_name_resample,'kernel':"cubic:0,0.5",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':spacing[2]/self._voxel_size}
+                    print tmpCommandCT
+                    subprocess.call(tmpCommandCT,shell=True)
+                elif self._resampling_method=='Registration':
+                    ct_image = sitk.ReadImage(ct_file_name)
+                    interpolator = InterpolateVolume(self._voxel_size)
+                    ct_interp=interpolator.execute(ct_image)
+                    sitk.WriteImage(ct_interp,ct_file_name_resample,True)
+                    #Now Fix x,y
+                    tmpCommandCT = tmpCommandUnuResampling % {'in':ct_file_name_resample,'out':ct_file_name_resample,'kernel':"tent",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':1.0}
+                    print tmpCommandCT
+                    subprocess.call(tmpCommandCT,shell=True)
+                elif self._resampling_method=='Hybrid':
+                    print "Hybrid resampling approach..."
+                    ct_image = sitk.ReadImage(ct_file_name)
+                    interpolator = InterpolateVolume(2.0*self._voxel_size)
+                    ct_interp=interpolator.execute(ct_image)
+                    sitk.WriteImage(ct_interp,ct_file_name_resample,True)
+                    #Now cubic for x,y and z
+                    tmpCommandCT = tmpCommandUnuResampling % {'in':ct_file_name_resample,'out':ct_file_name_resample,'kernel':"cubic:0,0.5",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':2.0}
+                    print tmpCommandCT
+                    subprocess.call(tmpCommandCT,shell=True)
+
+                tmpCommandPL = tmpCommandUnuResampling % {'in':pl_file_name,'out':pl_file_name_resample,'kernel':"cheap",'f1':spacing[0]/self._voxel_size,'f2':spacing[1]/self._voxel_size,'f3':spacing[2]/self._voxel_size}
+                print tmpCommandPL
                 subprocess.call(tmpCommandPL,shell=True)
+                
                 ct_file_name = ct_file_name_resample
                 pl_file_name = pl_file_name_resample
         
         
         for ii in self._regions:
-            rtag = ii.lower()
+            #Lower camel case region tag
+            rtag = ii[0].lower()+ii[1:]
             tmpDir = os.path.join(self._tmp_dir,rtag)
             if os.path.exists(tmpDir) == False:
                 os.mkdir(tmpDir)
             
             # Define FileNames that will be used
-            pl_file_nameRegion= os.path.join(tmpDir,self._case_id + "_" + rtag + "_partialLungLabelMap.nhdr")
-            ct_file_nameRegion= os.path.join(tmpDir,self._case_id + "_" + rtag + ".nhdr")
-            featureMapFileNameRegion = os.path.join(tmpDir,self._case_id + "_" + rtag + "_featureMap.nhdr")
-            maskFileNameRegion = os.path.join(tmpDir,self._case_id + "_" + rtag + "_mask.nhdr")
+            pl_file_nameRegion= os.path.join(tmpDir,self._case_id + "_" + rtag + "_partialLungLabelMap.nrrd")
+            ct_file_nameRegion= os.path.join(tmpDir,self._case_id + "_" + rtag + ".nrrd")
+            featureMapFileNameRegion = os.path.join(tmpDir,self._case_id + "_" + rtag + "_featureMap.nrrd")
+            maskFileNameRegion = os.path.join(tmpDir,self._case_id + "_" + rtag + "_mask.nrrd")
             particlesFileNameRegion = os.path.join(self._output_prefix+ "_" + rtag + "VesselParticles.vtk")
             
             if self._justparticles == False:
@@ -231,6 +265,8 @@ if __name__ == "__main__":
     parser.add_argument("--minscale",dest="min_scale",type=float,default=0.7)
     parser.add_argument("--maxscale",dest="max_scale",type=float,default=4)
     parser.add_argument("--init",dest="init_method",default="Frangi")
+    parser.add_argument("--vesselness_th",dest="vesselness_th",type=float,default=0.38)
+    parser.add_argument("--resampling",dest="resampling_method",default="Linear",help="Resampling method for CT image: linear, cubic, registration (demons-based approach), hybrid (registration + cubic)")
     parser.add_argument("--multires",dest="multires",action="store_true", default = False)
     parser.add_argument("--justparticles",dest="justparticles",action="store_true", default=False)
     parser.add_argument("--cleanCache", action="store_true", dest="clean_cache", default=False)
@@ -254,7 +290,7 @@ if __name__ == "__main__":
     crop = [int(kk) for kk in str.split(op.crop,',')]
     regions = [kk for kk in str.split(op.regions,',')]
     
-    vp=VesselParticlesPipeline(op.ct_file_name,op.pl_file_name,regions,op.tmp_dir,op.output_prefix,op.init_method,\
-                               op.lth,op.sth,op.voxel_size,op.min_scale,op.max_scale,crop,op.rate,op.multires,op.justparticles,op.clean_cache)
+    vp=VesselParticlesPipeline(op.ct_file_name,op.pl_file_name,regions,op.tmp_dir,op.output_prefix,op.init_method,op.resampling_method,\
+                               op.lth,op.sth,op.voxel_size,op.min_scale,op.max_scale,op.vesselness_th,crop,op.rate,op.multires,op.justparticles,op.clean_cache)
                                
     vp.execute()
