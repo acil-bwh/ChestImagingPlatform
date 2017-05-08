6a49c5b2519226b9fd6f04dca35b2df2fc3e984e
rola harmouche <rharmo@bwh.harvard.edu>
2017-05-08 17:28:38 -0400

    ENH: Adding projection images to lung QC, and other changes

--- cip_python/qualitycontrol/qc_base.py
+++ cip_python/qualitycontrol/qc_base.py
@@ -23,16 +23,18 @@ class QcColorConventionsManager:
     qc_color_mapping = None
     
     
-    @staticmethod
-    def applyConvention(conventionId):
-        if conventionId in QcColorConventionsManager.qc_color_mapping:
-            return QcColorConventionsManager.qc_color_mapping[conventionId]
-        else:
-            return QcColorConventionsManager.qc_background
+    #@staticmethod
+    #def applyConvention(conventionId):
+    #    if conventionId in QcColorConventionsManager.qc_color_mapping:
+    #        return QcColorConventionsManager.qc_color_mapping[conventionId]
+    #    else:
+    #        return QcColorConventionsManager.qc_background
 
     @staticmethod
     def buildColorMap(overlay_alpha = 0.85):    
+        
         my_colors = colors.ColorConverter()
+        
         QcColorConventionsManager.qc_color_mapping = {
         'LEFTSUPERIORLOBE':  my_colors.to_rgba('red', alpha=overlay_alpha),
         'LEFTINFERIORLOBE': my_colors.to_rgba('green', alpha=overlay_alpha),
@@ -45,17 +47,40 @@ class QcColorConventionsManager:
         'RIGHTMIDDLETHIRD': my_colors.to_rgba('yellow', alpha=overlay_alpha),
         'RIGHTLOWERTHIRD': my_colors.to_rgba('blue', alpha=overlay_alpha),
         'RIGHTUPPERTHIRD': my_colors.to_rgba('red', alpha=overlay_alpha),
-        }    
-                  
+        }  
+          
+        QcColorConventionsManager.qc_type_color_mapping = {
+        'Airway': my_colors.to_rgba('fuchsia', alpha=overlay_alpha),
+        'Vessel': my_colors.to_rgba('limegreen', alpha=overlay_alpha),
+        }                  
         color_bound_list = [[QcColorConventionsManager.qc_background, 0 ]]
         
+        """ append label value with region and type to color bounds list. (for now, either region or type)"""
         mychestConvenstion =ChestConventions()
         for region in QcColorConventionsManager.qc_color_mapping.keys():
             """ append the color to the color list"""
             """ get the label bounds and append to bounds list. The bounds are lblval:lblval+1"""
-            reg_val =  mychestConvenstion.GetChestRegionValueFromName(region)   
-            color_bound_list.append([QcColorConventionsManager.qc_color_mapping[region], reg_val])        
-        
+            reg_val =  mychestConvenstion.GetChestRegionValueFromName(region)  
+            regtypeval = mychestConvenstion.GetValueFromChestRegionAndType(reg_val, 0) 
+            color_bound_list.append([QcColorConventionsManager.qc_color_mapping[region], regtypeval])        
+
+        for the_type in QcColorConventionsManager.qc_type_color_mapping.keys():
+            """ append the color to the color list"""
+            """ get the label bounds and append to bounds list. The bounds are lblval:lblval+1"""
+            type_val =  mychestConvenstion.GetChestTypeValueFromName(the_type)   
+            regtypeval = mychestConvenstion.GetValueFromChestRegionAndType(0, type_val) 
+            color_bound_list.append([QcColorConventionsManager.qc_type_color_mapping[the_type], regtypeval])  
+
+        """ This is not the ideal way to have region/type colors"""
+        for region in QcColorConventionsManager.qc_color_mapping.keys():
+            reg_val =  mychestConvenstion.GetChestRegionValueFromName(region)
+            for the_type in QcColorConventionsManager.qc_type_color_mapping.keys():
+                type_val =  mychestConvenstion.GetChestTypeValueFromName(the_type) 
+                regtypeval = mychestConvenstion.GetValueFromChestRegionAndType(reg_val, type_val)
+                the_colortemp= (np.array(QcColorConventionsManager.qc_color_mapping[region])+np.array(QcColorConventionsManager.qc_type_color_mapping[the_type]))/2
+                the_color = np.ndarray.tolist(the_colortemp)
+                color_bound_list.append([the_color, regtypeval])  
+                                                   
         """ Sort the tuples and turn back to list """
         color_bound_list.sort(key=lambda tup: tup[1]) 
         color_bound_list = zip(*color_bound_list)
@@ -66,7 +91,6 @@ class QcColorConventionsManager:
             if ((color_bound_list[1][i+1] != color_bound_list[1][i]+1) and (color_bound_list[0][i] != QcColorConventionsManager.qc_background)):
                 color_bound_list[0].insert(i+1, QcColorConventionsManager.qc_background)
                 color_bound_list[1].insert(i+1, color_bound_list[1][i]+1)
-
         color_bound_list[0].append(QcColorConventionsManager.qc_background)
         color_bound_list[1].append(color_bound_list[1][-1]+1)              
         color_bound_list[0].append(QcColorConventionsManager.qc_background)
@@ -161,10 +185,58 @@ class ImageOverlay:
         projection = self.rotate_ct_images_for_display(projection, axis)
                 
         return(projection)
-         
+
+    def get_labelmap_projection(self, in_lm, regions, types, axis='axial'):
+        
+        """ project along axis"""   
+        if (axis=='sagittal'):  
+            axis_val=0
+        if (axis=='coronal'):  
+            axis_val=1              
+        if (axis=='axial'):  
+            axis_val=2    
+        
+        parser = RegionTypeParser(in_lm)
+
+        mychestConvenstion =ChestConventions()
+        mask = np.zeros_like(in_lm)
+        if(regions):
+            if mychestConvenstion.GetChestWildCardName() in regions:
+                #extract a labelmap with only chest types
+                mask = mychestConvenstion.GetChestRegionFromValue(in_lm)
+            else:
+                for i in range(0,np.shape(regions)[0]):             
+                    reg_val =  mychestConvenstion.GetChestRegionValueFromName(regions[i])
+                    mask = np.logical_or(mask, parser.get_mask(chest_region=reg_val) ) 
+        if(types):
+            if mychestConvenstion.GetChestWildCardName() in types:
+                #extract a labelmap with only chest types
+                mask = mychestConvenstion.GetChestTypeFromValue(in_lm)
+            else:
+                for i in range(0,np.shape(types)[0]):   
+                    type_val =  mychestConvenstion.GetChestTypeValueFromName(types[i])
+                    print(type_val)          
+                    mask = np.logical_or(mask, parser.get_mask(chest_type=type_val) )  
+        lm_masked = np.copy(in_lm)
+        lm_masked[mask==0]=0 
+        
+        """ if types=None then extract only regions, if regions=None then extract only types.
+        Else leave as is, the label values are a combination of regions and types."""
+        if(regions is None):
+            """ extract only types"""
+            lm_masked = mychestConvenstion.GetChestTypeFromValue(lm_masked)
+            lm_masked = mychestConvenstion.GetValueFromChestRegionAndType(np.zeros_like(lm_masked), lm_masked)
+        if(types is None):
+            lm_masked = mychestConvenstion.GetChestRegionFromValue(lm_masked)
+            
+        """  maximum intensity projection  """               
+        projection = np.max(lm_masked, axis=axis_val)
+        projection = self.rotate_labelmap_images_for_display(projection, axis)
+        return([projection])         
+
+                                                      
     def get_ct_overlay(self, in_ct1, in_ct2, in_mask, num_overlays, axis='axial', is_checkerboard = False):
-        #img = (1.0 - alpha)*fixed + alpha*moving
-    
+   
         xmax, ymax,zmax = np.max(np.where(in_mask>0), 1)
         xmin, ymin,zmin = np.min(np.where(in_mask>0), 1)
 
@@ -202,22 +274,42 @@ class ImageOverlay:
         return list_of_cts #overlay_images
         
                                              
-    def get_segmentation_overlay(self, in_ct, in_lm, num_overlays, regions, axis='axial'):
- 
+    def get_segmentation_overlay(self, in_ct, in_lm, num_overlays, regions, types, axis='axial'):
+
         parser = RegionTypeParser(in_lm)
-
-        mychestConvenstion =ChestConventions()
-        reg_val =  mychestConvenstion.GetChestRegionValueFromName(regions[0])
-
-        mask =  parser.get_mask(chest_region=reg_val) 
-        for i in range(1,np.shape(regions)[0]):             
-            reg_val =  mychestConvenstion.GetChestRegionValueFromName(regions[i])
-            mask = np.logical_or(mask, parser.get_mask(chest_region=reg_val) ) 
+        mychestConvenstion =ChestConventions()       
         
-        
+        """ TODO: Add wildcard"""
+        mask = np.zeros_like(in_lm)
+        if(regions):
+            if mychestConvenstion.GetChestWildCardName() in regions:
+                #extract a labelmap with only chest types
+                mask = mychestConvenstion.GetChestRegionFromValue(in_lm)
+            else:
+                for i in range(0,np.shape(regions)[0]):             
+                    reg_val =  mychestConvenstion.GetChestRegionValueFromName(regions[i])
+                    mask = np.logical_or(mask, parser.get_mask(chest_region=reg_val) ) 
+
+        if(types):
+            if mychestConvenstion.GetChestWildCardName() in types:
+                """ extract a labelmap with only chest regions""" 
+                mask = mychestConvenstion.GetChestTypeFromValue(in_lm)
+            else:
+                for i in range(0,np.shape(types)[0]):   
+                    type_val =  mychestConvenstion.GetChestTypeValueFromName(types[i])
+                    mask = np.logical_or(mask, parser.get_mask(chest_type=type_val) )                 
         lm_masked = np.copy(in_lm)
         lm_masked[mask==0]=0 
         
+        """ if types=None then extract only regions, if regions=None then extract only types.
+        Else leave as is... Need to have color mapping for regions and types combined"""
+        if(regions is None):
+            """ extract only types"""
+            lm_masked = mychestConvenstion.GetChestTypeFromValue(lm_masked)
+            lm_masked = mychestConvenstion.GetValueFromChestRegionAndType(np.zeros_like(lm_masked), lm_masked)
+        if(types is None):
+            lm_masked = mychestConvenstion.GetChestRegionFromValue(lm_masked)
+            
         xmax, ymax,zmax = np.max(np.where(lm_masked>0), 1)
         xmin, ymin,zmin = np.min(np.where(lm_masked>0), 1)
 
@@ -226,7 +318,7 @@ class ImageOverlay:
 
         bounds_lm = [xmin,xmax, ymin,ymax, zmin,zmax] 
         bounds_ct = [xminct, xmaxct, yminct,ymaxct, zminct,zmaxct ]
-        
+
         bounds_list = self.get_overlay_bounds_from_volume_bounds(bounds_ct, bounds_lm, num_overlays, axis)                                    
 
         """ Exract the list of overlays """
@@ -237,7 +329,7 @@ class ImageOverlay:
             ct_patch = np.squeeze(in_ct[bounds_list[i][0]:bounds_list[i][1],bounds_list[i][2]:bounds_list[i][3],\
                 bounds_list[i][4]:bounds_list[i][5]]).astype(float)
             
-            lm_patch = np.squeeze(in_lm[bounds_list[i][0]:bounds_list[i][1],bounds_list[i][2]:bounds_list[i][3],\
+            lm_patch = np.squeeze(lm_masked[bounds_list[i][0]:bounds_list[i][1],bounds_list[i][2]:bounds_list[i][3],\
                 bounds_list[i][4]:bounds_list[i][5]]).astype(float)            
 
             ct_patch = self.rotate_ct_images_for_display(ct_patch, axis)
@@ -288,9 +380,9 @@ class Montage:
         gs=GridSpec(spec_val,spec_val, wspace=0.01, hspace=0.01)
                
         QcColorConventionsManager.buildColorMap(overlay_alpha = overlay_alpha)
-        from matplotlib.colors import LinearSegmentedColormap
-        
         for i in range(0, num_rows):
+            num_columns=len(list_of_cts[i])
+            print(str(num_columns)+" num columns")
             for j in range(0, num_columns):
                 ax_1 = fig.add_subplot(gs[i:(i+1),j:(j+1)])
 
@@ -298,17 +390,15 @@ class Montage:
                 ax_1.set_yticks([])
                 ax_1.autoscale_view('tight')
                 fig.add_subplot(ax_1)
+                
                 num_ct_overlays = len(list_of_cts[i][j])
                 
-
                 for k in range(0, num_ct_overlays):
                     list_of_cts[i][j][k][list_of_cts[i][j][k]<window_level] = window_level
                     list_of_cts[i][j][k][list_of_cts[i][j][k]>(window_level+window_width)] = (window_level+window_width)
                     list_of_cts[i][j][k] = list_of_cts[i][j][k]-np.min(list_of_cts[i][j][k])
-                    list_of_cts[i][j][k] = list_of_cts[i][j][k]/float(np.max(list_of_cts[i][j][k]))  
+                    list_of_cts[i][j][k] = list_of_cts[i][j][k]/float(np.max(list_of_cts[i][j][k])+0.1)  
                        
-
-                #pdb.set_trace()
                 im_shape = np.shape(list_of_cts[i][j][0])        
                 if(list_of_voxel_spacing is None):
                     the_extent=None
@@ -374,50 +464,79 @@ class LabelmapQC:
         pass
         
     
-    def get_labelmap_qc(self, in_ct, out_file, list_of_labelmaps, list_request_qc_per_labelmap, num_images_per_region=3, 
-            overlay_alpha=0.85, window_width=1100, window_level=-1024, axis='axial', resolution=600, 
-            spacing=None):   
+    def get_labelmap_qc(self, in_ct, out_file, list_of_labelmaps, list_request_qc_per_labelmap, 
+            list_of_axes, num_images_per_region=3, overlay_alpha=0.85, window_width=1100, 
+            window_level=-1024, resolution=600,  spacing=None, is_overlay=False, is_projection=False):   
 
     
         """ the layout of the images should be set here"""
         append_horizontally = False
             
         list_of_images = []
-        list_cts = []
+        list_cts = [] 
         list_labelmaps = []
         list_of_voxel_spacing=[]
-        
-        """ partial lung labelmap images"""
+        print("getting labelmap QC")
+        print(list_request_qc_per_labelmap)
+
         for j in range (0, len(list_of_labelmaps)):
             for i in range(0, len(list_request_qc_per_labelmap[j])):
-                #list_of_images.append([]) 
-                in_regions = self.region_dictionary[list_request_qc_per_labelmap[j][i]]
+                
+                if self.region_dictionary[list_request_qc_per_labelmap[j][i]]:
+                    in_regions= self.region_dictionary[list_request_qc_per_labelmap[j][i]]
+                else:
+                    in_regions = None
+                if self.type_dictionary[list_request_qc_per_labelmap[j][i]]:
+                    in_types = self.type_dictionary[list_request_qc_per_labelmap[j][i]]
+                else:
+                    in_types = None
+                    
                 overlay = ImageOverlay()
-                
-                print("generating overlay for the following regions: ")
-                print(in_regions)
+                if ((is_overlay) and (list_request_qc_per_labelmap[j][i] in self.overlay_qc)):
+                                    
+                    print("generating overlay for the following regions: ")
+                    print(in_regions)
+                    print(in_types)
   
-                #list_of_images[i] = overlay.execute( in_ct, list_of_labelmaps[j],num_images_per_region, in_regions) 
-                temp_list_cts,temp_list_labelmaps = overlay.get_segmentation_overlay( in_ct, list_of_labelmaps[j],num_images_per_region, in_regions, axis=axis)
+                    """ get overlay QC"""
+                    temp_list_cts,temp_list_labelmaps = overlay.get_segmentation_overlay( in_ct, list_of_labelmaps[j],\
+                        num_images_per_region, in_regions, in_types, axis=list_of_axes[j][i])
 
-                list_cts.append(temp_list_cts)
-                list_labelmaps.append(temp_list_labelmaps)
+                    list_cts.append(temp_list_cts)
+                    list_labelmaps.append(temp_list_labelmaps)
+                     
+                    if(spacing):    
+                        if (list_of_axes[j][i]=='axial'): 
+                            list_of_voxel_spacing.append([[spacing[1],spacing[0]]]*num_images_per_region) 
+                        elif(list_of_axes[j][i]=='sagittal'): 
+                            list_of_voxel_spacing.append([[spacing[2],spacing[1]]]*num_images_per_region) 
+                        elif(list_of_axes[j][i]=='coronal'): 
+                            list_of_voxel_spacing.append([[spacing[2],spacing[0]]]*num_images_per_region)                 
                 
-                #im_shape = np.shape(temp_list_labelmaps[0]) 
-                if(spacing):    
-                    if (axis=='axial'): 
-                        list_of_voxel_spacing.append([[spacing[1],spacing[0]]]*num_images_per_region) 
-                    elif(axis=='sagittal'): 
-                        list_of_voxel_spacing.append([[spacing[2],spacing[1]]]*num_images_per_region) 
-                    elif(axis=='coronal'): 
-                        list_of_voxel_spacing.append([[spacing[2],spacing[0]]]*num_images_per_region) 
-                    #list_of_images.append(overlay.execute( in_ct, list_of_labelmaps[j],num_images_per_region, in_regions, axis=axis)) 
+                #""" get projection QC """
+                if((is_projection) and (list_request_qc_per_labelmap[j][i] in self.projection_qc)):
 
-            if (num_images_per_region==1):
-                append_horizontally = True
+                    print("generating projections for: ")
+                    print(in_regions)
+                    print(in_types)
+                                        
+                    overlay_projection = overlay.get_labelmap_projection(list_of_labelmaps[j], in_regions, in_types, axis=list_of_axes[j][i])
+                    #temp_list_cts,temp_list_labelmaps = overlay.get_segmentation_overlay( in_ct, list_of_labelmaps[j],\
+                    #    1, in_regions, in_types, axis=list_of_axes[j][i])
+                        
+
+                    
+                    if(spacing):    
+                        if (list_of_axes[j][i]=='axial'): 
+                            list_of_voxel_spacing.append([[spacing[1],spacing[0]]]*2) 
+                        elif(list_of_axes[j][i]=='sagittal'): 
+                            list_of_voxel_spacing.append([[spacing[2],spacing[1]]]*2) 
+                        elif(list_of_axes[j][i]=='coronal'): 
+                            list_of_voxel_spacing.append([[spacing[2],spacing[0]]]*2) 
+                    list_cts.append([np.zeros_like(overlay_projection)])
+                    list_labelmaps.append([overlay_projection])
+                    
+        return [list_cts, list_labelmaps, list_of_voxel_spacing]
+
        
-        print("generating montage")
-        #pdb.set_trace()
-        my_montage = Montage() 
-        my_montage.execute(list_cts,list_labelmaps, out_file, overlay_alpha, len(list_cts), num_images_per_region, \
-            window_width=window_width, window_level=window_level, resolution=resolution, list_of_voxel_spacing=list_of_voxel_spacing)      
+       
