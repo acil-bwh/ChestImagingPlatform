4206c77d46a20f4367617103ad39c1f061a89e1e
rola harmouche <rharmo@bwh.harvard.edu>
2017-06-11 22:59:27 -0400

    ENH: change chest partitions to output a dictionary of requested partitions. Reworked that class structure

--- cip_python/segmentation/rind_vs_core_partition.py
+++ cip_python/segmentation/rind_vs_core_partition.py
@@ -3,44 +3,25 @@ import pdb
 import copy
 import scipy.ndimage.morphology as scimorph
 from cip_python.segmentation.chest_partition import ChestPartition
-from cip_python.segmentation.chest_partition import ChestPartitionRegionConventions
 
 
 class RindVsCorePartition(ChestPartition):
     """General class for rind versus core partition generation.
     
-    Attributes
-    ----------
-    partition_regions_ : list of strings
-                          Name of the regions that come with a partition     
-
-    rind_windth_ : float
-                    Width of the rind in mm
-    rind_ratio_ : float
-                  ratio between the rind width and the maximum width possible  
-                  Either rind_windth_ or rind_ratio_ must be set. Not both.  
     """
-    def __init__(self, rind_width = None, rind_ratio = None):
+    def __init__(self): #, rind_width = None, rind_ratio = None):
         """
         """
-        
-        assert ((rind_width is None) or (rind_ratio is None)), \
-            "rind_windth and rind_ratio cannot both be set " 
 
-        assert ((rind_width is not None) or (rind_ratio is not None)), \
-            "Either rind_windth and rind_ratio must be set " 
-            
-        self.rind_width_ = rind_width
-        self.rind_ratio_ = rind_ratio
         ChestPartition.__init__(self) 
         
     def declare_partition_regions(self):
-        partition_regions = ['Rind', 'Core']
+        partition_regions = ['Rind10','Core10','Rind15','Core15','Rind20','Core20']
         
         return partition_regions
        
-    def execute(self, lung_labelmap, spacing):
-        
+    def execute(self, lung_labelmap, spacing, chest_partitions):
+
         """
         compute the labelmap containing the partition regions
         
@@ -51,34 +32,80 @@ class RindVsCorePartition(ChestPartition):
         spacing : array, shape ( 3 )
             The x, y, and z spacing, respectively, of the labelmap    
         
+        
+        chest_partitions: If
+            left unspecified both here and in the constructor, then the
+            complete set of entities found in the label map will be used.
+
         Returns
         -------
-        None  
+        partition_labelmaps: dict()  
+            Dictionary of labelmaps for all the requested partitions.
         """
         
-        cp = ChestPartitionRegionConventions()
         
-        # Compute the distance map associated with lung_labelmap
+        """ Compute the distance map associated with lung_labelmap """
         binary_labelmap = copy.copy(lung_labelmap)
-        binary_labelmap[binary_labelmap > 0] = 1
+        binary_labelmap[binary_labelmap > 0] = 1 
         
         # distance_transform_edt returns 0 whele labelmap is 0 and +ve
         # distance everywhere else 
         lung_distance_map = \
             scimorph.distance_transform_edt(binary_labelmap)
-            
-        if self.rind_width_ is not None:
-            rind_width = self.rind_width_
-        else:
-            rind_width = lung_distance_map.max()*self.rind_ratio_
         
-        # Convert the rind width from mm to voxels
-        rind_width_voxels = rind_width/spacing[0]
+        partition_labelmaps=dict()
+        """ return 1 labelmap per requested partition. These should be boolean"""   
+        for partition_name in chest_partitions:
+            if partition_name == self.get_partition_wildcard_name():
+                partition_labelmaps['WildCard']=np.zeros(np.shape(lung_labelmap), dtype=bool)
+                partition_labelmaps['WildCard'][binary_labelmap > 0] = 1  
+ 
+            else:
+                assert partition_name in self.partition_regions_, \
+                  "Invalid partition name " + partition_name
 
-        self.partition_labelmap_ = np.zeros_like(lung_labelmap)
-        #pdb.set_trace()
-        self.partition_labelmap_[(lung_distance_map<=rind_width_voxels)] = cp.get_partition_region_value_from_name('Rind')
-        self.partition_labelmap_[(lung_distance_map>rind_width_voxels) ] = cp.get_partition_region_value_from_name('Core')
-        
-        self.partition_labelmap_[lung_labelmap==0]=0
+                if partition_name == 'Rind10':
+                    rind_width=10.0  
+                    rind_width_voxels = rind_width/spacing[0]                                      
+                    partition_labelmaps['Rind10']= np.zeros(np.shape(lung_labelmap), dtype=bool)#np.zeros_like(lung_labelmap)
+                    partition_labelmaps['Rind10'][(lung_distance_map<=rind_width_voxels)]=1 
+                    partition_labelmaps['Rind10'][lung_labelmap==0]=0 
+
+                elif partition_name == 'Core10':
+                    rind_width=10.0  
+                    rind_width_voxels = rind_width/spacing[0]                                         
+                    partition_labelmaps['Core10']= np.zeros(np.shape(lung_labelmap), dtype=bool)#np.zeros_like(lung_labelmap)
+                    partition_labelmaps['Core10'][(lung_distance_map>rind_width_voxels)]=1 
+                    partition_labelmaps['Core10'][lung_labelmap==0]=0                 
+
+                elif partition_name == 'Rind15':
+                    rind_width=15.0  
+                    rind_width_voxels = rind_width/spacing[0]                                      
+                    partition_labelmaps['Rind15']= np.zeros(np.shape(lung_labelmap), dtype=bool)#np.zeros_like(lung_labelmap)
+                    partition_labelmaps['Rind15'][(lung_distance_map<=rind_width_voxels)]=1 
+                    partition_labelmaps['Rind15'][lung_labelmap==0]=0 
+    
+                elif partition_name == 'Core15':
+                    rind_width=15.0  
+                    rind_width_voxels = rind_width/spacing[0]                                         
+                    partition_labelmaps['Core15']= np.zeros(np.shape(lung_labelmap), dtype=bool)
+                    partition_labelmaps['Core15'][(lung_distance_map>rind_width_voxels)]=1 
+                    partition_labelmaps['Core15'][lung_labelmap==0]=0            
+            
+                elif partition_name == 'Rind20':
+                    rind_width=20.0  
+                    rind_width_voxels = rind_width/spacing[0]                                      
+                    partition_labelmaps['Rind20']= np.zeros(np.shape(lung_labelmap), dtype=bool)
+                    partition_labelmaps['Rind20'][(lung_distance_map<=rind_width_voxels)]=1 
+                    partition_labelmaps['Rind20'][lung_labelmap==0]=0 
+    
+                elif partition_name == 'Core20':
+                    rind_width=20.0  
+                    rind_width_voxels = rind_width/spacing[0]                                         
+                    partition_labelmaps['Core20']= np.zeros(np.shape(lung_labelmap), dtype=bool)
+                    partition_labelmaps['Core20'][(lung_distance_map>rind_width_voxels)]=1 
+                    partition_labelmaps['Core20'][lung_labelmap==0]=0          
+            
+        return partition_labelmaps
+                
         
