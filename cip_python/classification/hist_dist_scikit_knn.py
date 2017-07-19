DEB: fix issue when hist sumis 0 in kde computation

cip_python/classification/hist_dist_scikit_knn.py
@@ -136,14 +136,15 @@ class HistDistScikitKNN():
         #    classification_data[0,hist.shape[0]] = dist               
         
 
-        print(hist.shape)
-        print(np.shape(hist.shape))
+        #print(hist.shape)
+        #print(np.shape(hist.shape))
         
         if (np.shape(hist.shape)[0] > 1):
-            print("using hist")
+            #print("using hist")
             subset_indeces = self.scikit_knn.kneighbors(hist, return_distance=False)[0]
         else:
             subset_indeces = self.scikit_knn.kneighbors([hist], return_distance=False)[0]
+            
         subset_training_histogram = self.hists_[subset_indeces]
         subset_training_distance = self.dists_[subset_indeces]
         subset_training_class = self.y_[subset_indeces]
@@ -151,8 +152,7 @@ class HistDistScikitKNN():
         my_knn_classifier2 = HistDistKNN(n_neighbors = self.n_neighbors_ , beta = self.beta_) #HistDistKNN
         my_knn_classifier2.fit(subset_training_histogram, subset_training_distance, subset_training_class)
         class_label = my_knn_classifier2.predict(hist, dist)
-            
-                                                
+                                                          
         return class_label
         
 