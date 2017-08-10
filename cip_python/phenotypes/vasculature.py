5d734203b9e97eef1c0dd7eebc678faddd7c10db
jonieva <jorgeonieva@gmail.com>
2017-08-10 10:29:28 -0400

    ENH: Remove uses of 'Agg' - 2

--- cip_python/phenotypes/vasculature.py
+++ cip_python/phenotypes/vasculature.py
@@ -8,10 +8,6 @@ from scipy.stats import kde
 from scipy.integrate import quadrature
 import os.path
 
-import matplotlib
-#Use Agg backend to allow non-interactive rendering 
-#without relying on X-windows. Other options are: PDF, Cairo..
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from cip_python.phenotypes import Phenotypes
