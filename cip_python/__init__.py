5d66d25dde127e6e70e712756f745b502fd2d948
jonieva <jorgeonieva@gmail.com>
2017-08-17 12:08:16 -0400

    Disable full package import in cip_python
    Not all the tools that use cip_python will need all the packages (ex:
    dcnn tools). If something, cip_common and input_output may be needed.
    But it probably doesn’t make much sense to enable only those ones,
    because anyway the individual subpackages are generally imported
    individually

--- cip_python/__init__.py
+++ cip_python/__init__.py
@@ -1,16 +1,11 @@
-import common
-import input_output
-import utils
-import classification
-import phenotypes
-import particles
-import visualization
-import qualitycontrol
-import segmentation
-import registration
-
-import os
-if os.sys.platform == "win32":
-  print ("Warning: nipype not available in Windows")
-else:
-  import nipype
+# import common
+# import input_output
+# import utils
+# import classification
+# import phenotypes
+# import particles
+# import visualization
+# import qualitycontrol
+# import segmentation
+# import registration
+# import nipype
