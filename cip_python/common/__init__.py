3780a1827c74f2843da1223d9899691421c147ba
jonieva <jorgeonieva@gmail.com>
2018-01-21 12:59:57 -0500

    ENH: DeepLearningModelsManager and LungSegmentation use case / 2

--- cip_python/common/__init__.py
+++ cip_python/common/__init__.py
@@ -4,4 +4,4 @@ from .chest_conventions import ChestConventions
 from .chest_conventions_static import ChestRegion, ChestType, ImageFeature, Plane, ReturnCode
 from .geometry_topology_data import GeometryTopologyData, BoundingBox, Point
 from .anatomic_structures_manager import AnatomicStructuresManager
-#from .deep_learning_models_manager import *
+from .deep_learning_models_manager import *
---
+++ cip_python/common/deep_learning_models_manager.py
@@ -0,0 +1,111 @@
+import os
+import os.path as osp
+import requests
+import sys
+
+class DeepLearningModelsManager(object):
+    # Model keys
+    LUNG_SEGMENTATION_AXIAL = 'LungSegmentationAxial'
+    LUNG_SEGMENTATION_CORONAL = 'LungSegmentationCoronal'
+
+    # Model URLs
+    _MODELS_ = {
+          LUNG_SEGMENTATION_AXIAL: 'lung_segmentation/axial.h5',
+          LUNG_SEGMENTATION_CORONAL: 'lung_segmentation/coronal.h5'
+    }
+
+    def __init__(self, root_url="https://s3.amazonaws.com/acil-deep-learning-data/models/"):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        root_url: root url where all the models will be searched
+
+        """
+        self._ROOT_URL_ = root_url
+
+    def get_model(self, key):
+        """
+        Get a model local path. Download the model if it has not been already downloaded
+        Parameters
+        ----------
+        key: model key
+
+        Returns
+        -------
+        Path to a local file that contains a model that may have been downloaded from a remote location
+        """
+        if key not in self._MODELS_:
+            raise Exception("Model '{}' is not one of the allowed values. Allowed values: {}".format(key,
+                                                                                 self._MODELS_.keys()))
+        local_model_path = osp.realpath(osp.join(self._get_models_root_local_folder_(), self._MODELS_[key]))
+        url = self._ROOT_URL_ + self._MODELS_[key]
+        if not osp.isfile(local_model_path):
+            # Download the model
+            print ("Downloading the model to {}...".format(local_model_path))
+            if not self._download_model_(url, local_model_path):
+                raise Exception("Model file could not be retrieved")
+            print ("Model {} downloaded".format(key))
+        return local_model_path
+
+    def _get_models_root_local_folder_(self):
+        """
+        Get the local folder where the models will be stored.
+        The folder will be created if needed
+        Returns
+        -------
+
+        """
+        real_path = osp.realpath(osp.join(osp.dirname(__file__), "dcnn_models_tmp"))
+        if not osp.isdir(real_path):
+            try:
+                os.makedirs(real_path)
+            except Exception as ex:
+                raise Exception("Models folder could not be created ('{}'): {}".format(real_path, ex))
+        return real_path
+
+
+    def _download_model_(self, url, local_file):
+        """
+        Download a model from a remote location
+
+        Parameters
+        ----------
+        key: model key
+        local_file: full local path where the model is going to be stored
+
+        Returns
+        -------
+        True if the process went good or False of the process failed
+
+        """
+        try:
+
+            local_folder = osp.dirname(local_file)
+            if not osp.isdir(local_folder):
+                os.makedirs(local_folder)
+                print ("{} folder created".format(local_folder))
+
+            # NOTE the stream=True parameter
+            r = requests.get(url, stream=True)
+            total_size = 0
+            with open(local_file, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=10000):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                        f.flush()
+                    total_size+=len(chunk)
+
+            if os.stat(local_file).st_size < 4000:
+                # There seems to be an error here. The file size should be way bigger
+                with open(local_file, 'r') as f:
+                    r = f.read()
+                print("Error when downloading file: {}".format(r))
+                # Remove wrong file
+                os.remove(local_file)
+                return False
+            return True
+        except Exception as ex:
+            print ("There was an error while downloading the model: ", ex)
+            return False
