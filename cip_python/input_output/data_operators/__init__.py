c38b3b4b03a54d862f94d0731f008c602fb3a5c6
PietroNardelli <pnardelli@bwh.harvard.edu>
2019-07-25 12:44:01 -0400

    ADD: adding random rotation operator

--- cip_python/input_output/data_operators/__init__.py
+++ cip_python/input_output/data_operators/__init__.py
@@ -2,3 +2,4 @@ from . data_operator import *
 from . uniform_affine_2D_transform_DO import UniformAffineTransform2DDataOperator
 from . gaussian_noise_DO import GaussianNoiseDataOperator
 from . uniform_similarity_3D_transform_with_coords_DO import UniformSimilarity3DTransformWithCoordsDataOperator
+from . random_rotation_DO import RandomRotatioDataOperator
