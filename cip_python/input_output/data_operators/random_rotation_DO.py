e2fcbd0ef1c93b7e9bba6907554794c855f4e11b
PietroNardelli <pnardelli@bwh.harvard.edu>
2019-07-25 12:41:26 -0400

    ADD: adding random rotation operator

---
+++ cip_python/input_output/data_operators/random_rotation_DO.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.ndimage.interpolation import rotate
+
+from . import DataOperatorInterface
+
+
+class RandomRotatioDataOperator(DataOperatorInterface):
+    def __init__(self, max_rotation_angle, min_rotation_angle, rotation_axis=(1, 0), fill_mode='nearest', reshape=False):
+        self.max_rotation_angle = max_rotation_angle
+        self.min_rotation_angle = min_rotation_angle
+        self.rotation_axis = rotation_axis
+        self.fill_mode = fill_mode
+        self.reshape = reshape
+
+        self.rotation_angle = None
+
+    def set_operation_parameters(self, rotation_angle):
+        self.rotation_angle = rotation_angle
+
+    def run(self, data, generate_parameters=True):
+        if generate_parameters:
+            self.rotation_angle = np.random.randint(low=self.min_rotation_angle, high=self.max_rotation_angle)
+        else:
+            assert self.rotation_angle is not None, "Rotation angle not specified"
+
+        if isinstance(data, np.ndarray):
+            return rotate(data, self.rotation_angle, axes=self.rotation_axis, mode=self.fill_mode,
+                          reshape=self.reshape)
+        elif isinstance(data, list):
+            result = list()
+            for image in data:
+                result.append(rotate(image, self.rotation_angle, axes=self.rotation_axis, mode=self.fill_mode,
+                                     reshape=self.reshape))
+            return result
+        else:
+            raise AttributeError("Wrong type for 'data'. It should be an image (numpy array) or a list of images")
