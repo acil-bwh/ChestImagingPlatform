ae47b482a8e855ab207daedee89cd99c619daf4b
PietroNardelli <pnardelli@bwh.harvard.edu>
2019-11-12 12:37:26 -0500

    ENH: small change to dice coefficient computation

--- cip_python/dcnn/logic/metrics_manager.py
+++ cip_python/dcnn/logic/metrics_manager.py
@@ -59,12 +59,12 @@ class MetricsManager(object):
     ###########################################################################################################
     @classmethod
     def dice_coef(cls, y_true, y_pred):
-        smooth = 0.0000001
+        smooth = 1.
         y_true_f = K.flatten(y_true)
         y_pred_f = K.flatten(y_pred)
         intersection = K.sum(y_true_f * y_pred_f)
         # return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-        return (2. * intersection) / (K.sum(y_true_f ** 2) + K.sum(y_pred_f ** 2) + smooth)
+        return (2. * intersection + smooth) / (K.sum(K.square(y_true_f)) + K.sum(K.square(y_pred_f)) + smooth)
 
     @classmethod
     def dice_coef_loss(cls, y_true, y_pred):
