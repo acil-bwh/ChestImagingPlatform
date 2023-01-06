622fe3b869981540beb31bcd2b8d775bbd5ecd83
Ariel H. Curiale <curiale@gmail.com>
2023-01-06 14:25:05 -0500

    ENH: Returns the dense field from Txs #48

---
+++ cip_python/registration/composite_transform.py
@@ -0,0 +1,39 @@
+"""
+File: composite_transform.py
+Author: Ariel Hernán Curiale
+Email: curiale@gmail.com
+Github: https://gitlab.com/Curiale
+Description:
+    This class composes a list of ITK transfomration into a single composite
+    ITK transform
+"""
+import SimpleITK as sitk
+
+
+class CompositeTransform():
+    """
+    Composite transforms represents a stack of transformtation. The
+    transforms are composed in reverse order with the back being applied first.
+    For example, (T0 o T1) (x) = T0(T1(x)) an the transforms are stored in a
+    queue as [T0, T1, ....]. the method AddTransform() adds the transforms to
+    the back of the queue. For example let be A the affine part of a complex
+    free form deformation, T, and F the pure free form deformation without an
+    affine component, i.e F x = A^-1 T x. Now T x = A F x, so to compose a
+    complet tx T, from a pure ffd and an affine tranasformation we need to do:
+    T = A o F
+
+    comp = CompisteTransform()
+    comp.AddTransform(affine)      ----> [affine]
+    comp.AddTransform(ffd)   ----> [affine, ffd]
+
+    comp.TransformPoint(x)      ----> affine(ffd(x))
+
+    Note: The first tx added is the last to be applied.
+    """
+
+    def __new__(self, tx):
+        dim = tx[0].GetDimension()
+        composite = sitk.CompositeTransform(dim)
+        for tx_i in tx:
+            composite.AddTransform(tx_i)
+        return composite
---
+++ cip_python/registration/tx2dfield.py
@@ -0,0 +1,74 @@
+"""
+File: tx2dfield.py
+Author: Ariel Hernán Curiale
+Email: curiale@gmail.com
+Github: https://gitlab.com/Curiale
+Description:
+    This class return the dense deformation field from a list of
+    transformations
+"""
+import SimpleITK as sitk
+from .composite_transform import CompositeTransform
+
+
+class Transform2DenseField():
+    """
+    Filter to create a dense deformation field from a list of ITK Transform
+    """
+
+    def __init__(self, tx, ref_img):
+        if not isinstance(tx, list):
+            tx = list(tx)
+        if len(tx) == 1:
+            self.tx = tx[0]
+        else:
+            self.tx = CompositeTransform(tx)
+        self.ref = ref_img
+
+    def Execute(self):
+        """
+        Returns the dense Displacement Field
+        """
+        dfield_filter = sitk.TransformToDisplacementFieldFilter()
+        dfield_filter.SetReferenceImage(self.ref)
+        dfield = dfield_filter.Execute(self.tx)
+        return dfield
+
+
+def main():
+    import argparse
+    usage = "usage: given a image reference and a lis of ITK transformations, "
+    usage += " create the dense deformation field.\n"
+    usage += "NOTE: First Tx passed is the last Tx applied, for emxaple, the"
+    usage += " orden should be [Affine, FFD]"
+    parser = argparse.ArgumentParser(description=usage)
+    parser.add_argument('-r',
+                        '--ref',
+                        help="Reference image",
+                        type=str,
+                        required=True)
+    parser.add_argument('-s',
+                        '--save',
+                        help="Save file",
+                        type=str,
+                        required=True)
+    parser.add_argument(
+        '-t',
+        '--transforms',
+        nargs='+',
+        help="List of transformations, for example [Affine, FFD]",
+        type=list,
+        required=True)
+
+    args = parser.parse_args()
+    # Read image
+    ref_img = sitk.ReadImage(args.ref)
+    # Read ITK Transfomrations
+    tx = [sitk.ReadTransform(tx_name) for tx_name in args.transforms]
+    tx2dfield = Transform2DenseField(tx, ref_img)
+    dfield = tx2dfield.Execute()
+    sitk.WriteImage(dfield, args.save, True)
+
+
+if __name__ == "__main__":
+    main()
