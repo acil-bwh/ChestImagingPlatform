0484dbccbfdfa307ab9c8003f200c156a4edb7fe
Pietro <pn964@mgb031796.partners.org>
2024-01-16 16:50:55 -0500

    ENH: adding option to dilate labelmap after skeletonization

--- cip_python/utils/skeletonize_labelmap.py
+++ cip_python/utils/skeletonize_labelmap.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 from skimage.morphology import skeletonize
+import SimpleITK as sitk
 
 from cip_python.input_output.image_reader_writer import ImageReaderWriter
 
@@ -9,6 +10,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Method to reduce label map to 1 pixel wide representations')
     parser.add_argument('--i_lm', help="Path to labelmap (.nrrd)", type=str, required=True)
     parser.add_argument('--o', help="Path to save skeletonized labelmap (.nrrd)", type=str, required=True)
+    parser.add_argument("--dilate", help='Optional. Set to dilate skeletonized label', action="store_true")
+    parser.add_argument("--r", help='Radius for dilation', required=False, nargs='+', type=str,
+                        default=[1, 1, 1])
     args = parser.parse_args()
 
     io = ImageReaderWriter()
@@ -17,9 +21,10 @@ if __name__ == "__main__":
     in_lm[in_lm > 1] = 1
     out_lm = skeletonize(in_lm)
 
+    if args.dilate:
+        radius = [int(i) for i in args.r]
+
+        out_lm_sitk = sitk.BinaryDilate(sitk.GetImageFromArray(out_lm), radius, kernelType=sitk.sitkCross)
+        out_lm = sitk.GetArrayFromImage(out_lm_sitk)
+
     io.write_from_numpy(out_lm, metainfo, args.o)
-
-
-
-
-
