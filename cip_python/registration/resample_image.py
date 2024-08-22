f77b4d3a65756d909a04748ba2a77a71ac84c2ba
Raul San Jose <rjosest@gmail.com>
2024-08-22 20:09:28 +0200

    ENH: Expand functionality to enable inversion of affine matrix. Add selection of interpolator as option

--- cip_python/registration/resample_image.py
+++ cip_python/registration/resample_image.py
@@ -1,15 +1,8 @@
-"""
-File: resample_image.py
-Author: Ariel Hernán Curiale
-Email: curiale@gmail.com
-Github: https://gitlab.com/Curiale
-Description:
-    This script resample an image according to the alist of tx
-"""
 
 import SimpleITK as sitk
+import re
 from cip_python.registration.transform import CompositeTransform
-from cip_python.registration.tx2field import Transform2DenseField
+from cip_python.registration.tx2dfield import Transform2DenseField
 
 
 def cast(img, pixelID):
@@ -78,8 +71,10 @@ class ResampleImage():
             interpolator = sitk.sitkLinear
         if is_label:
             interpolator = sitk.sitkNearestNeighbor
+
         img_warpped = sitk.Resample(self.mov, ref_img, self.tx, interpolator,
                                     default_value)
+
         if intensity_correction:
             pixelID = img_warpped.GetPixelID()
             use_img_spacing = True
@@ -105,13 +100,13 @@ def main():
     usage = (
         "usage: given fixed and moving images, and a lis of "
         "ITK transformations, this script resample the moving image into the "
-        "fixed image.\n"
+        "coordinate space defined by the reference image.\n"
         "NOTE: First Tx passed is the last Tx applied, for emxaple, the"
         " orden should be [Affine, FFD]")
     parser = argparse.ArgumentParser(description=usage)
-    parser.add_argument('-f',
-                        '--fix',
-                        help="Fix image",
+    parser.add_argument('-r',
+                        '--ref',
+                        help="Reference image",
                         type=str,
                         required=True)
     parser.add_argument('-m',
@@ -119,9 +114,9 @@ def main():
                         help="Moving image",
                         type=str,
                         required=True)
-    parser.add_argument('-s',
-                        '--save',
-                        help="Save file",
+    parser.add_argument('-o',
+                        '--output',
+                        help="Resampled moving image file",
                         type=str,
                         required=True)
     parser.add_argument(
@@ -131,20 +126,55 @@ def main():
         help="List of transformations, for example [Affine, FFD]",
         type=str,
         required=True)
+    parser.add_argument("--interpolator", dest="interpolator", choices=['NearestNeighbor','Linear','BSpline','BSpline1','BSpline2','BSpline3','BSpline4','BSpline5',
+        'WelchWindowedSinc','HammingWindowedSinc','CosineWindowedSinc','BlackmanWindowedSinc','LanczosWindowSinc'],default='Linear')
     parser.add_argument('-c', '--correction', action='store_true')
-    parser.add_argument('-o', '--offset_correction', type=float, default=0)
+    parser.add_argument('-oc', '--offset_correction', type=float, default=0)
 
     args = parser.parse_args()
     # Read images
-    fix = sitk.ReadImage(args.fix)
+    fix = sitk.ReadImage(args.ref)
     mov = sitk.ReadImage(args.moving)
     # Read ITK Transfomrations
-    tx = [sitk.ReadTransform(tx_name) for tx_name in args.transforms]
+    #Read transforms
+    tx=list()
+    T_filenames=args.transforms
+    for tt in args.transforms:
+        res = re.findall(r'\[(.*?)\]', tt)
+        if len(res)==0:
+            t_file=tt
+        elif len(res)>=1:
+            res=res[0].split(',')
+            t_file=res[0]
+        T_tmp=sitk.ReadTransform(t_file)
+
+        if len(res)>1:
+            if res[1]=='1':
+                #Invert transform
+                print("Inverting transform")
+                T_tmp=T_tmp.GetInverse()
+        tx.append(T_tmp)
+
+    #tx = [sitk.ReadTransform(tx_name) for tx_name in args.transforms]
+
+    #Interpolator map
+    interpolator=dict()
+    interpolator['NearestNeighbor']=sitk.sitkNearestNeighbor
+    interpolator['Linear']=sitk.sitkLinear
+    interpolator['BSpline']=sitk.sitkBSpline
+    interpolator['BSpline1']=sitk.sitkBSpline1
+    interpolator['BSpline2']=sitk.sitkBSpline2
+    interpolator['BSpline3']=sitk.sitkBSpline3
+    interpolator['BSpline4']=sitk.sitkBSpline4
+    interpolator['BSpline5']=sitk.sitkBSpline5
+    interpolator['WelchWindowedSinc']=sitk.sitkWelchWindowedSinc
+    interpolator['HammingWindowedSinc']=sitk.sitkHammingWindowedSinc
+
 
     resample = ResampleImage(fix, mov, tx)
     warpped = resample.Execute(intensity_correction=args.correction,
-                               offset_correction=args.offset_correction)
-    sitk.WriteImage(warpped, args.save, True)
+                               offset_correction=args.offset_correction,interpolator=interpolator[args.interpolator])
+    sitk.WriteImage(warpped, args.output, True)
 
 
 if __name__ == "__main__":
